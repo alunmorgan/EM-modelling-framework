5afb6d9e0a5007330c0e713a19c2f1e3ff64637c
Alun Morgan <alun.morgan@diamond.ac.uk>
2016-11-22 18:09:32 +0000

    Used str2func to be able to programmatically call different test functions / models. This enables a large reduction in code, and what is left is more general.

--- Test_suite/Testing_top_level.m
+++ Test_suite/Testing_top_level.m
@@ -6,7 +6,7 @@
 % All are strings.
 %
 % For the simulation and postprocessing:
-% input_file_loc => The path to the directory containing the model 
+% input_file_loc => The path to the directory containing the model
 %                   definition files.
 % scratch_loc => The path to be used for storing temporary files generated
 %                during the simulation.
@@ -25,110 +25,62 @@
 
 
 load_local_paths
-
+%  start = datenum('20161010T120000', 'yyyymmddTHHMMSS');
+%  fin = now;
 %% Setting up which versions of GdfidL to use.
-versions = {'150717g','150719g', '150923g', '151128g', '160410g','160429g','160517g'};
-%versions = {'160517g'};
+ versions = {'150717g','150719g', '150923g', '151128g', '160410g',...
+             '160429g','160517g', '161003g', '161108g'};
+% versions = {'161108g'};
+log = {'Log of problems'};
+
+model_names = {'cylindrical_pillbox', ...
+    'cylindrical_pillbox_with_port',...
+    'cylindrical_pillbox_with_port',...
+    'cylindrical_pillbox_with_racetrack',...
+    'cylindrical_pillbox_with_racetrack_with_port',...
+    'cylindrical_pillbox_with_racetrack_with_4ports'};
 
 %% Running the tests
-try
-    start = now;
+start = now;
+for ha = 1:length(model_names)
     for kse = 1:length(versions)
-        Testing_cylindrical_pillbox(input_file_loc, scratch_loc, data_loc, versions{kse})
+        try
+            Testing_function = str2func(['Testing_', model_names{ha}]);
+            Testing_function(input_file_loc, scratch_loc,...
+                data_loc, versions{kse})
+        catch
+            log{end+1} = ['Running model ',model_names{ha}, ' failed on version ', versions{kse}];
+            warning(['Running model ', model_names{ha}, ' failed'])
+        end
     end
+    
     fin = now;
-    output_loc = [results_loc,'cylindrical_pillbox'];
-    postprocessor_setup('cylindrical_pillbox', start, fin, ...
-        scratch_loc, data_loc, results_loc, versions)
-    Report_setup(Author, 'TEST', graphic_loc, output_loc, start, fin, 'w')
-    arc_names = GdfidL_find_selected_models(output_loc, {start, fin});
-    Blend_reports( 'Test comparison report', [output_loc,'/'], ...
-        arc_names , Author, 'TEST_blend', graphic_loc)
-catch
-    warning('Test cylindrical_pillbox failed')
+    try
+        postprocessor_setup(model_names{ha}, start, fin, ...
+            scratch_loc, data_loc, results_loc)
+    catch
+        log{end+1} = ['Postprocessing ', model_names{ha}, 'failed'];
+        warning(['Postprocessing ', model_names{ha}, 'failed'])
+    end
 end
 
-try
-    start = now;
-    for kse = 1:length(versions)
-        Testing_cylindrical_pillbox_with_port(input_file_loc, scratch_loc, data_loc, versions{kse})
-    end
-    fin = now;
-    output_loc = [results_loc,'cylindrical_pillbox_with_port'];
-    postprocessor_setup('cylindrical_pillbox_with_port', start, fin, ...
-        scratch_loc, data_loc, results_loc, versions)
-    Report_setup(Author, 'TEST', graphic_loc, output_loc, start, fin, 'w')
-    arc_names = GdfidL_find_selected_models(output_loc, {start, fin});
-    Blend_reports( 'Test comparison report', [output_loc,'/'], ...
-        arc_names , Author, 'TEST_blend', graphic_loc)
-catch
-    warning('Test cylindrical_pillbox_with_port failed')
+disp('Generating the individual reports')
+% Generate all the new reports
+for md = 1:length(model_names)
+    output_loc = [results_loc,model_names{md}];
+    model_name_for_report = regexprep(model_names{md}, '_', ' ');
+    Report_setup(Author, ['Test suite ', model_name_for_report ], graphic_loc, output_loc, start, fin)
 end
 
-try
-    start = now;
-    for kse = 1:length(versions)
-        Testing_cylindrical_pillbox_with_4ports(input_file_loc, scratch_loc, data_loc, versions{kse})
-    end
-    fin = now;
-    output_loc = [results_loc,'cylindrical_pillbox_with_4ports'];
-    postprocessor_setup('cylindrical_pillbox_with_4ports', start, fin, ...
-        scratch_loc, data_loc, results_loc, versions)
-    Report_setup(Author, 'TEST', graphic_loc, output_loc, start, fin, 'w')
+disp('Generating the combined reports') 
+% Generate all the blended reports using all available data.
+start= datenum('20150101T180000', 'yyyymmddTHHMMSS');
+for md = 1:length(model_names)
+    output_loc = [results_loc,model_names{md}];
     arc_names = GdfidL_find_selected_models(output_loc, {start, fin});
+    model_name_for_report = regexprep(model_names{md}, '_', ' ');
     Blend_reports( 'Test comparison report', [output_loc,'/'], ...
-        arc_names , Author, 'TEST_blend', graphic_loc)
-catch
-    warning('Test cylindrical_pillbox_with_4ports failed')
-end
-
-try
-    start = now;
-    for kse = 1:length(versions)
-        Testing_cylindrical_pillbox_with_racetrack(input_file_loc, scratch_loc, data_loc,versions{kse})
-    end
-    fin = now;
-    output_loc = [results_loc,'cylindrical_pillbox_with_racetrack'];
-    postprocessor_setup('cylindrical_pillbox_with_racetrack', start, fin, ...
-        scratch_loc, data_loc, results_loc, versions)
-    Report_setup(Author, 'TEST', graphic_loc, output_loc, start, fin, 'w')
-    arc_names = GdfidL_find_selected_models(output_loc, {start, fin});
-    Blend_reports( 'Test comparison report', [output_loc,'/'], ...
-        arc_names , Author, 'TEST_blend', graphic_loc)
-catch
-    warning('Test cylindrical_pillbox_with_racetrack failed')
-end
-
-try
-    start = now;
-    for kse = 1:length(versions)
-        Testing_cylindrical_pillbox_with_racetrack_with_port(input_file_loc, scratch_loc, data_loc, versions{kse})
-    end
-    fin = now;
-    output_loc = [results_loc,'cylindrical_pillbox_with_racetrack_with_port'];
-    postprocessor_setup('cylindrical_pillbox_with_racetrack_with_port', start, fin, ...
-        scratch_loc, data_loc, results_loc, versions)
-    Report_setup(Author, 'TEST', graphic_loc, output_loc, start, fin, 'w')
-    arc_names = GdfidL_find_selected_models(output_loc, {start, fin});
-    Blend_reports( 'Test comparison report', [output_loc,'/'], ...
-        arc_names , Author, 'TEST_blend', graphic_loc)
-catch
-    warning('Test cylindrical_pillbox_with_racetrack_with_port failed')
-end
-
-try
-    start = now;
-    for kse = 1:length(versions)
-        Testing_cylindrical_pillbox_with_racetrack_with_4ports(input_file_loc, scratch_loc, data_loc, versions{kse})
-    end
-    fin = now;
-    output_loc = [results_loc,'cylindrical_pillbox_with_racetrack_with_4ports'];
-    postprocessor_setup('cylindrical_pillbox_with_racetrack_with_4ports', start, fin, ...
-        scratch_loc, data_loc, results_loc, versions)
-    Report_setup(Author, 'TEST', graphic_loc, output_loc, start, fin, 'w')
-    arc_names = GdfidL_find_selected_models(output_loc, {start, fin});
-    Blend_reports( 'Test comparison report', [output_loc,'/'], ...
-        arc_names , Author, 'TEST_blend', graphic_loc)
-catch
-    warning('Test cylindrical_pillbox_with_racetrack_with_4ports failed')
+        arc_names , Author, ...
+        ['Test suite ', model_name_for_report, 'core behaviour check' ],...
+        graphic_loc)
 end
