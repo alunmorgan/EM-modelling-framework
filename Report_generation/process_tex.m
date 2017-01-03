0030cbe8aacc01ff743a19cdaa9e6ab4b3bf82ba
Alun Morgan <alun.morgan@diamond.ac.uk>
2017-01-03 17:16:59 +0000

    Changed code so output is sent to a log file rather than the command line. Warnings are raised if there is a problem with the latex processing. The call to latex has also been changed so that it does not expect human input upon finding an error.

--- Report_generation/process_tex.m
+++ Report_generation/process_tex.m
@@ -4,13 +4,34 @@ function process_tex(output_path, file_name)
 % example: process_tex(output_path, file_name)
 old_path = pwd;
 cd(output_path)
-system(['latex -etex ', file_name,'.tex'])
-system(['latex -etex ', file_name,'.tex'])
-system(['latex -etex ', file_name,'.tex'])
-system(['dvipdf ', file_name,'.dvi'])
+fprintf('Processing latex file. ')
+latex_cmd = 'latex -etex -interaction nonstopmode -halt-on-error ';
+[status(1), log] = system([latex_cmd, file_name,'.tex']);
+fprintf('.')
+[status(2), log] = system([latex_cmd, file_name,'.tex']);
+fprintf('.')
+[status(3), log] = system([latex_cmd, file_name,'.tex']);
+fprintf('.')
+file_ID = fopen([file_name, '_latex_log'], 'w');
+fwrite(file_ID,log);
+fclose(file_ID);
+if sum(status) >0
+    warning(['Error in latex processing. See ', output_path, '/', file_name, '_latex_log for details'])
+end
+[conversion_status, ~] = system(['dvipdf ', file_name,'.dvi']);
+fprintf('. ')
+if conversion_status >0
+    warning('Error in converting dvi to pdf')
+end
+disp('Cleaning up')
 delete([file_name, '.dvi'])
+fprintf('20%% ')
 delete([file_name, '.aux'])
+fprintf('40%% ')
 delete([file_name, '.log'])
+fprintf('60%% ')
 delete([file_name, '.out'])
+fprintf('80%% ')
 delete([file_name, '.toc'])
+fprintf('100%% \n')
 cd(old_path)
