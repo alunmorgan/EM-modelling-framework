4d210c1890844586427faf82806593b4c171a45e
Alun Morgan <alun.morgan@diamond.ac.uk>
2017-06-20 12:05:52 +0100

    Fixed problem where the previous change to the input data structure was not reflected in the S-parameter code path.

--- Run_models/construct_s_param_gdf_file.m
+++ Run_models/construct_s_param_gdf_file.m
@@ -1,31 +1,41 @@
-function construct_s_param_gdf_file(mi, modelling_inputs, port_name)
+function construct_s_param_gdf_file(input_file_path, modelling_inputs, port_name)
 % Write the input gdf file for an S-paramter simulation of the requested
 % model.
 %
-% mi is 
-% modelling_inputs is
+% input_file_path is the path to the geometry model gdf file.
+% modelling_inputs is the structure containing the input data for a single
+% simulation run.
 % port_name is
 %
 % Example: construct_s_param_gdf_file(mi, modelling_inputs, port_name)
+if ~isempty(modelling_inputs.mat_list)
+    materials = modelling_inputs.mat_list(:,1);
+    material_labels = modelling_inputs.mat_list(:,2);
+    material_override = find_material_overrides(...
+        materials, modelling_inputs.defs);
+else
+    material_labels = [];
+    material_override = [];
+end %if
 
-mesh = mi.mesh_stepsize;
-materials = mi.mat_list(:,1);
-material_labels = mi.mat_list(:,2);
-in_path = mi.input_file_path;
-model_name = mi.model_name;
-add_defs = modelling_inputs.defs;
-num_threads = mi.n_cores;
-excitation_f = mi.s_param_excitation_f;
-excitation_bw = mi.s_param_excitation_bw;
-tmax = mi.s_param_tmax;
 
-material_override = find_material_overrides(materials,  add_defs);
-model_file = [in_path, model_name, '_model_data'];
+fs = gdf_s_param_header_construction('', 'temp', ...
+       modelling_inputs.NPMLs,...
+       modelling_inputs.n_cores,...
+       modelling_inputs.mesh_stepsize,...
+       material_override,...
+       material_labels);
+   
+   
+model_file = fullfile(input_file_path, ...
+    [modelling_inputs.model_name, '_model_data']);
 data = read_file_full_line(model_file);
-fs = gdf_s_param_header_construction('', 'temp', num_threads, mesh,...
-       material_override, material_labels);
-mon = gdf_s_param_monitor_construction(port_name, excitation_f, excitation_bw, tmax);
+
+mon = gdf_s_param_monitor_construction(port_name,...
+    modelling_inputs.s_param_excitation_f, ...
+    modelling_inputs.s_param_excitation_bw, ...
+    modelling_inputs.s_param_tmax);
 % construct the full input file.
-data = cat(1,fs, add_defs', data, mon);
+data = cat(1,fs, modelling_inputs.defs', data, mon);
 % write the full datafile to base_path.
-write_out_data( data, strcat('temp_data/','model.gdf') )
+write_out_data( data, 'temp_data/model.gdf')
