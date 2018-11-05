8bd92f3c85c0fe20bc7e98c33cbd30864dea7639
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-11-05 14:20:13 +0000

    Combined the two material loss extraction codes.

--- Postprocess_models/extract_material_losses_from_wake_data.m
+++ Postprocess_models/extract_material_losses_from_wake_data.m
@@ -1,4 +1,4 @@
-function [model_mat_data, mat_loss] = extract_material_losses_from_wake_data(wake_data, extension_names)
+function [model_mat_data, mat_loss, m_time, m_data] = extract_material_losses_from_wake_data(wake_data, extension_names)
 % wake data (structure): contains all the data from the wake postprocessing
 %
 % mat_loss (vector): Returns a value of total loss per material.
@@ -25,4 +25,22 @@ if isfield(wake_data.raw_data, 'mat_losses')
             end %for    
         end %if
     end %if
+end %if
+
+% Evolution over time
+if isfield(wake_data.raw_data, 'mat_losses')
+    if isempty(wake_data.raw_data.mat_losses.loss_time) == 0
+        for na = size(model_mat_data,1):-1:1
+            if isempty(model_mat_data{na,4})
+                m_time{na} = 0;
+                m_data{na} = 0;
+            else
+                m_time{na} = model_mat_data{na,4}(:,1).*1e9;
+                m_data{na} = model_mat_data{na,4}(:,2).* 1e9;
+            end %if
+        end %for    
+    end %if
+else
+    m_time = NaN;
+    m_data = NaN;
 end %if
