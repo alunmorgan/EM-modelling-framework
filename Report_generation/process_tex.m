7c2dfb9bdb6e33327d0dfe8e69f0bec7cfe4b6cb
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-04-17 19:18:28 +0100

    Changed to work under windows

--- Report_generation/process_tex.m
+++ Report_generation/process_tex.m
@@ -5,7 +5,7 @@ function process_tex(output_path, file_name)
 old_path = pwd;
 cd(output_path)
 fprintf(['Processing latex file. ', output_path, '/',file_name])
-latex_cmd = 'latex -etex -interaction nonstopmode -halt-on-error ';
+latex_cmd = '"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" -etex -interaction nonstopmode -halt-on-error ';
 [status(1), log] = system([latex_cmd, file_name,'.tex']);
 fprintf('.')
 [status(2), log] = system([latex_cmd, file_name,'.tex']);
@@ -18,20 +18,27 @@ fclose(file_ID);
 if sum(status) >0
     warning(['Error in latex processing. See ', output_path, '/', file_name, '_latex_log for details'])
 end
-[conversion_status, ~] = system(['dvipdf ', file_name,'.dvi']);
-fprintf('. ')
-if conversion_status >0
-    warning('Error in converting dvi to pdf')
-end
+if exist([file_name, '.dvi'],'file') ~= 0
+    [conversion_status, ~] = system(['dvipdf ', file_name,'.dvi']);
+    fprintf('. ')
+    if conversion_status >0
+        warning('Error in converting dvi to pdf')
+    end
+end %if
 disp('Cleaning up')
-delete([file_name, '.dvi'])
-fprintf('20%% ')
-delete([file_name, '.aux'])
-fprintf('40%% ')
-delete([file_name, '.log'])
-fprintf('60%% ')
-delete([file_name, '.out'])
-fprintf('80%% ')
-delete([file_name, '.toc'])
-fprintf('100%% \n')
+if exist([file_name, '.dvi'],'file') ~= 0
+    delete([file_name, '.dvi'])
+end %if
+if exist([file_name, '.aux'],'file') ~= 0
+    delete([file_name, '.aux'])
+end %if
+if exist([file_name, '.log'],'file') ~= 0
+    delete([file_name, '.log'])
+end %if
+if exist([file_name, '.out'],'file') ~= 0
+    delete([file_name, '.out'])
+end %if
+if exist([file_name, '.toc'],'file') ~= 0
+    delete([file_name, '.toc'])
+end %if
 cd(old_path)
