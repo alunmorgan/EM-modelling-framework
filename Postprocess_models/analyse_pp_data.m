de83578135bb5216250e4a8fccc2276735e8b56d
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-05-16 13:16:51 +0100

    Adding helper functions to organise post processing the output of the modelling code.

---
+++ Postprocess_models/analyse_pp_data.m
@@ -0,0 +1,27 @@
+function analyse_pp_data(root_path, model_sets)
+
+for sts = 1:length(model_sets)
+    files = dir_list_gen_tree(fullfile(root_path, model_sets{sts}), 'mat', 1);
+    wanted_files = files(contains(files, 'data_postprocessed.mat'));
+    
+    for ind = 1:length(wanted_files)
+        current_folder = fileparts(wanted_files{ind});
+        if ~isfile(fullfile(current_folder, 'data_analysed_wake.mat'))
+        
+        load(fullfile(current_folder, 'data_postprocessed'), 'pp_data');
+        load(fullfile(current_folder, 'pp_inputs.mat'), 'ppi');
+        load(fullfile(current_folder, 'data_from_run_logs.mat'), 'run_logs');
+        load(fullfile(current_folder, 'run_inputs.mat'), 'modelling_inputs');
+        wake_data = wake_analysis(pp_data, ppi, modelling_inputs, run_logs);
+        
+        save(fullfile(current_folder, 'data_analysed_wake.mat'), 'wake_data','-v7.3')
+        disp(['Analysed ', current_folder])
+        clear 'pp_data' 'ppi' 'run_logs' 'modelling_inputs' 'wake_data',
+        else
+            disp(['Analysis for ', current_folder, ' already exists... Skipping'])
+        end %if
+    end %for
+end %for
+
+
+
---
+++ Postprocess_models/plotting/generate_graphs.m
@@ -0,0 +1,17 @@
+function generate_graphs(path_to_data)
+load(fullfile(path_to_data, 'wake', 'run_inputs.mat'),'modelling_inputs');
+if contains(modelling_inputs.sim_select, 'w')
+    GdfidL_plot_wake(path_to_data, 1E7)
+end %if
+if contains(modelling_inputs.sim_select, 'e')
+    GdfidL_plot_eigenmode(pp_data, path_to_data)
+end %if
+if contains(modelling_inputs.sim_select, 'l')
+    GdfidL_plot_eigenmode_lossy(pp_data, path_to_data)
+end %if
+if contains(modelling_inputs.sim_select, 's')
+    GdfidL_plot_s_parameters(path_to_data, fig_pos);
+end %if
+if contains(modelling_inputs.sim_select, 't')
+    GdfidL_plot_shunt(pp_data, path_to_data)
+end %if
