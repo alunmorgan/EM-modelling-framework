fa56d3666643b33883016164134fd7823dbef7bc
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-05-16 13:22:04 +0100

    Helper function to generate reports from analysed data.

---
+++ Report_generation/generate_report.m
@@ -0,0 +1,14 @@
+function generate_report(root_path, model_sets)
+
+for hfa = 1:length(model_sets)
+    files = dir_list_gen_tree(fullfile(root_path, model_sets{hfa}), 'mat', 1);
+    wanted_files = files(contains(files, 'data_postprocessed.mat'));
+    split_str = regexp(wanted_files, ['\',filesep], 'split');
+    for ind = 1:length(wanted_files)
+        model_name = split_str{ind}{end - 2};
+        path_to_data = fullfile(split_str{ind}{1:end-2});
+        disp(['Generating a report for ', model_name])
+        generate_graphs(path_to_data)
+        Report_setup(path_to_data)
+    end %for
+end %for
