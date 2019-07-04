aff38c188421e88bb2444ea0f1a03f890aa2452d
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-07-04 14:38:06 +0100

    Update get_wlf.m

--- Postprocess_models/get_wlf.m
+++ Postprocess_models/get_wlf.m
@@ -1,13 +1,8 @@
 function get_wlf(root, model_sets)
 
-model_names = {};
-wlf = {};
-for kfn = 1:length(model_sets)
-    disp(['************ ', model_sets{kfn}, ' ***************'])
-    [model_names{kfn}, wlf{kfn}, wake_length{kfn}, metric{kfn}] = extract_all_wlf(root, model_sets{kfn});
-    for ind = 1:length(model_names{kfn})
-        disp([model_names{kfn}{ind}, ' ', num2str(wlf{kfn}(ind) *1E-9),...
-            'mV/pC Wakelength ', num2str(wake_length{kfn}(ind)),...
-            ' Metric: ', num2str(metric{kfn}(ind)), '%'])
-    end %for
+[model_names, wlf, wake_length, metric] = extract_all_wlf(root, model_sets);
+for kfn = 1:length(model_names)
+        disp([model_names{kfn}, ' ', num2str(wlf(kfn) *1E-9),...
+            'mV/pC Wakelength ', num2str(wake_length(kfn)),...
+            ' Metric: ', num2str(metric(kfn)), '%'])
 end %for
