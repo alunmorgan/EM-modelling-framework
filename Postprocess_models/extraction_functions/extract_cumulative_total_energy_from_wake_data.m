9436c1484946f47e984b5226b0f516d6e805a094
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-07-04 14:48:25 +0100

    Update extract_cumulative_total_energy_from_wake_data.m Simplifying the calculation as most of teh heavy lifting is done in other functions and doen't need to be replecated here.

--- Postprocess_models/extraction_functions/extract_cumulative_total_energy_from_wake_data.m
+++ Postprocess_models/extraction_functions/extract_cumulative_total_energy_from_wake_data.m
@@ -6,16 +6,11 @@ function [timebase_cs, e_total_cs, e_ports_cs] =  extract_cumulative_total_energ
 % e_total_cs (vector): cumulative sum of the total energy in the structure (nJ).
 % e_ports_cs (vector): cumulative sum of the energy transmitted out of the ports (nJ).
 
-
 if wake_data.port_time_data.total_energy ~=0
-    t_step = wake_data.port_time_data.timebase(2) - wake_data.port_time_data.timebase(1);
-    for jsff = length(wake_data.port_time_data.data):-1:1 % number of ports
-        tmp = sum(wake_data.port_time_data.data{jsff},2);
-        e_ports_cs(:,jsff) = cumsum(tmp.^2) * t_step * 1e9;
-    end
-    e_total_cs = sum(e_ports_cs,2) * 1e9;
+    e_ports_cs = wake_data.port_time_data.port_energy_cumsum' .* 1e9;
+    e_total_cs = wake_data.port_time_data.total_energy_cumsum .* 1e9;
+    timebase_cs = wake_data.port_time_data.timebase * 1e9;
 else
     e_ports_cs = zeros(1,length(wake_data.port_time_data.data));
     e_total_cs = 0;
 end
-timebase_cs = wake_data.port_time_data.timebase *1e9;
