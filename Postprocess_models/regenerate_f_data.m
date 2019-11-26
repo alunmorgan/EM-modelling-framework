8da2a42070dcf8e8eff2c446293d4d243c82abff
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-11-26 15:28:51 +0000

    simplified port analysis.

--- Postprocess_models/regenerate_f_data.m
+++ Postprocess_models/regenerate_f_data.m
@@ -38,44 +38,49 @@ time_stepsize = abs(timescale(2) - timescale(1));
 % Create the corresponding frequency scale.
 n_freq_points = length(timescale);
 f_raw = (linspace(0,1,n_freq_points) / time_stepsize)';
+% Remove all data above the highest frequency of interest (hfoi).
+% Could either take the appropriate section from both the upper and lowver
+% side of the FFT, or just multiply by sqrt(2) as it is an amplitude.
+% (and ignore the small error due to counting DC twice).
+% Doing the truncation after each variable is created in order to reduce
+% the maximum memory footprint.
+hf_ind = find(f_raw > hfoi, 1, 'first');
+f_raw = f_raw(1:hf_ind);
+
 % Calculating the bunch spectra for a 1C charge.
 bunch_spectra = fft(Charge_distribution)/n_freq_points;
+Charge_distribution = []; % Cannot use clear in a parfor loop but I need to reduce the size.
+bunch_spectra = bunch_spectra(1:hf_ind) .*sqrt(2);
+
 fft_wp = fft(wakepotential)/n_freq_points;
+wakepotential = [];% Cannot use clear in a parfor loop but I need to reduce the size.
+fft_wp = fft_wp(1:hf_ind);
 % In order to get the proper impedence you need divide the fft of
 %the wake potential by the bunch spectrum. We take the real as this
 %corresponds to resistive losses.
 wakeimpedance =  - real(fft_wp ./ bunch_spectra);
 wakeimpedance_IM =  - imag(fft_wp ./ bunch_spectra);
 
-% Remove all data above the highest frequency of interest (hfoi).
-% Could either take the appropriate section from both the upper and lowver
-% side of the FFT, or just multiply by sqrt(2) as it is an amplitude.
-% (and ignore the small error due to counting DC twice).
-hf_ind = find(f_raw > hfoi, 1, 'first');
-f_raw = f_raw(1:hf_ind);
-bunch_spectra = bunch_spectra(1:hf_ind) .*sqrt(2);
-wakeimpedance = wakeimpedance(1:hf_ind);
-wakeimpedance_IM = wakeimpedance_IM(1:hf_ind);
-
+fft_wp = []; % Cannot use clear in a parfor loop but I need to reduce the size.
 
 if iscell(port_data)
-    for nsf = length(port_data):-1:1% number of ports
-        if sum(isnan(port_data{nsf})) > 0
-            port_impedances = NaN;
-            port_mode_fft{nsf} = NaN;
+    for nsf = 1:length(port_data)% number of ports
+        single_port = port_data{nsf};
+        if sum(isnan(single_port)) > 0
+            port_impedances_one_mode = NaN;
+            %             port_mode_fft = NaN;
         else
-            port_data{nsf} = cat(1,port_data{nsf},zeros(length(timescale) - ...
-                size(port_data{nsf},1), size(port_data{nsf},2)));
+            single_port = cat(1,single_port,zeros(length(timescale) - ...
+                size(single_port,1), size(single_port,2)));
             % Calculating the fft of the port signals
-            port_mode_fft_temp = fft(port_data{nsf},[],1)./n_freq_points;
-            % truncating to below the higest frequency of interest.
-            port_mode_fft{nsf} = port_mode_fft_temp(1:hf_ind,:);
-            clear port_mode_fft_temp
-            if isempty(cut_off_freqs) == 0
-                % setting all values below the cutoff frequency for each port and mode to
-                % zero.
-                for esns = length(cut_off_freqs{nsf}):-1:1 % number of modes
-                    tmp_ind = find(f_raw < cut_off_freqs{nsf}(esns),1,'last');
+            for kef = 1:size(single_port, 2) % number of modes
+                port_mode_fft_temp = fft(single_port(:,kef))./n_freq_points;
+                % truncating to below the higest frequency of interest.
+                port_mode_fft_temp = port_mode_fft_temp(1:hf_ind);
+                if isempty(cut_off_freqs) == 0
+                    % setting all values below the cutoff frequency for each port and mode to
+                    % zero.
+                    tmp_ind = find(f_raw < cut_off_freqs{nsf}(kef),1,'last');
                     % TEST
                     % move the index n samples above cutoff to reduce the effect of
                     % phase runaway. (set to zero now as I think that the error
@@ -83,31 +88,28 @@ if iscell(port_data)
                     % remove. If it is a gibbs like phenomina then the integral will be
                     % correct so the energy losses and power will be correct even if
                     % the cumulative sum graphs show some drops.)
-%                     n = 0;
-%                     tmp_ind = tmp_ind +n;
                     if tmp_ind < length(f_raw)
-                        %end_ind = length(f_raw);
-                        port_mode_fft{nsf}(1:tmp_ind,  esns) = 0;
-                        %port_mode_fft{nsf}(end_ind - tmp_ind+2:end_ind,  esns) = 0;
+                        port_mode_fft_temp(1:tmp_ind) = 0;
                     end %if
-                    clear tmp_ind
-                end %for
-                clear esns n
-            end %if
+                end %if
+                port_mode_fft(:,kef) = port_mode_fft_temp;
+                port_mode_fft_temp= []; % Cannot use clear in a parfor loop but I need to reduce the size.
+%                 single_port(:,kef) = [];% Cannot use clear in a parfor loop but I need to reduce the size.
+            end
+            
             % The transfer impedance at the ports is found using P = I^2 * Z. So the
             % impedance is the power seen at the ports divided by the bunch spectra squared.
             %     We take the real part as this corresponds to resistive losses.
             % take the abs because.....not quite sure if that is correct,however the
             % port impedance is oscillating around 0 otherwise.????
-            port_impedances(:,nsf) = real(sum(abs(port_mode_fft{nsf}).^2,2)) ./...
+            port_impedances_one_mode = real(sum(abs(port_mode_fft).^2,2)) ./...
                 abs(bunch_spectra).^2;
         end %if
-    end %for
-%     if sum(size(port_impedances) == [1, 1]) == 0
-%         port_impedances = port_impedances(1:hf_ind,:);
-%     end
+        port_impedances(:,nsf) = port_impedances_one_mode;
+        port_impedances_one_mode = [];% Cannot use clear in a parfor loop but I need to reduce the size.
+    end %parfor
 else
-     port_impedances = NaN;
+    port_impedances = NaN;
 end %if
 
 
