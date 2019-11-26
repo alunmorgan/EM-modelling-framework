c3acf965bd71be6d27a7aa87254c85cb579a53bc
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-11-26 15:19:42 +0000

    Changed the check of valid data to make it more robust.

--- Run_models/gdf_write_port_definitions.m
+++ Run_models/gdf_write_port_definitions.m
@@ -7,26 +7,30 @@ fs = cat(1,fs, '    -ports');
 fs = cat(1,fs,'        name = port_bp_in');
 fs = cat(1,fs,'        plane = zlow');
 fs = cat(1,fs,'        npml = NPMLs');
-fs = cat(1,fs,'        modes = 20');
+fs = cat(1,fs,'        modes = 40');
 fs = cat(1,fs,'        doit');
 fs = cat(1,fs,' ');
 fs = cat(1,fs,'    -ports');
 fs = cat(1,fs,'        name = port_bp_out');
 fs = cat(1,fs,'        plane = zhigh');
 fs = cat(1,fs,'        npml = NPMLs');
-fs = cat(1,fs,'        modes = 20');
+fs = cat(1,fs,'        modes = 40');
 fs = cat(1,fs,'        doit');
 fs = cat(1,fs,' ');
 
 for prt = 1:length(names)
     fs = cat(1,fs,'    -ports');
     fs = cat(1,fs,['        name = ', names{prt}]);
-    fs = cat(1,fs,['        plane = ', planes{prt}{1}]);
-    if length(planes{prt}) >1
-        for nea = 2:length(planes{prt})
-            fs = cat(1,fs,['        ', planes{prt}{nea}]);
+    if iscell(planes{prt})
+        fs = cat(1,fs,['        plane = ', planes{prt}{1}]);
+        if length(planes{prt}) >1
+            for nea = 2:length(planes{prt})
+                fs = cat(1,fs,['        ', planes{prt}{nea}]);
+            end %for
         end %if
-    end %for
+    else
+        fs = cat(1,fs,['        plane = ', planes{prt}]);
+    end %if
     fs = cat(1,fs,'        npml = NPMLs');
     fs = cat(1,fs,['        modes = ', num2str(modes(prt))]);
     fs = cat(1,fs,'        doit');
