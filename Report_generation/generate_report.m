46f5b51bc8a8091817af95105aa9f2804e78d90c
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-01-29 14:59:18 +0000

    Code now copes with both Unix and windows filesystem slashes.

--- Report_generation/generate_report.m
+++ Report_generation/generate_report.m
@@ -3,10 +3,14 @@ function generate_report(root_path, ppi, model_sets, port_overrides, chosen_wake
 for hfa = 1:length(model_sets)
     files = dir_list_gen_tree(fullfile(root_path, model_sets{hfa}), 'mat', 1);
     wanted_files = files(contains(files, 'data_postprocessed.mat'));
-    split_str = regexp(wanted_files, ['\',filesep], 'split');
+    split_str = regexp(wanted_files, filesep, 'split');
     for ind = 1:length(wanted_files)
         model_name = split_str{ind}{end - 2};
         path_to_data = fullfile(split_str{ind}{1:end-2});
+        if isempty(split_str{1,1}{1})
+            % This will ensure the leading slash is kept.
+            path_to_data = [filesep, path_to_data];
+        end %if
         disp(['Generating a report for ', model_name])
         generate_graphs(path_to_data, ppi, chosen_wake_length, hfoi)
         Report_setup(path_to_data, ppi, port_overrides, chosen_wake_length)
