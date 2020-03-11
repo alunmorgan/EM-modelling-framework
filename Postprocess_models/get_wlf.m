5af58045c413e8592d20c2404a712c1a38268bcd
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-03-11 09:50:24 +0000

    Removed unwated details from table.

--- Postprocess_models/get_wlf.m
+++ Postprocess_models/get_wlf.m
@@ -1,24 +1,20 @@
 function get_wlf(root, model_sets)
 
-[model_names, wlf, wlf_3mm, wlf_10mm, wake_length_t, mesh_stepsize, mesh_scaling, decay_to, simulation_time_t] = extract_all_wlf(root, model_sets);
+[model_names, wlf, wlf_3mm, wlf_10mm, wake_length_t, mesh_stepsize, mesh_scaling, n_cores, simulation_time_t] = extract_all_wlf(root, model_sets);
 for kfn = 1:size(model_names,1)
     names = model_names(kfn, :)';
     wlf_1mm = wlf(kfn,:)' .* 1E-9;
     wlf_3mm = wlf_3mm(kfn,:)' .* 1E-9;
     wlf_10mm = wlf_10mm(kfn,:)' .*1E-9;
     wake_length = wake_length_t(kfn,:)';
-    decay = decay_to(kfn, :)';
+    n_cores = n_cores(kfn, :)';
     simulation_time = simulation_time_t(kfn,:)' ./ 3600;
     mesh_stepsize = mesh_stepsize(kfn,:)' ./ mesh_scaling(kfn,:)' * 1e6;
-    T = table(names, wlf_1mm, wlf_3mm, wlf_10mm, wake_length, mesh_stepsize, simulation_time, decay);
+    T = table(names, wlf_1mm, wlf_3mm, wlf_10mm, wake_length, mesh_stepsize, simulation_time, n_cores);
     disp(T)
-%     for nfe = 1:size(model_names,2)
-%         disp([model_names{kfn, nfe}, ' ', num2str(wlf(kfn, nfe) *1E-9),...
-%             'mV/pC ', num2str(wlf_3mm(kfn, nfe) *1E-9),...
-%             'mV/pC ', num2str(wlf_10mm(kfn, nfe) *1E-9),...
-%             'mV/pC Wakelength ', num2str(wake_length(kfn, nfe)),...
-%             ' Decay level: ', num2str(decay_to(kfn, nfe)), ...
-%             ' Simulation time: ', num2str(simulation_time(kfn, nfe)), 's ',...
-%             ' Mesh density: ', num2str(mesh_density(kfn, nfe))])
-%     end %for
 end %for
+
+bar(n_cores, simulation_time ./ min(simulation_time))
+title('Finding optimum core selection')
+xlabel('Number of cores')
+ylabel('Multiple of minimum runtime')
--- Report_generation/extract_all_wlf.m
+++ Report_generation/extract_all_wlf.m
@@ -1,4 +1,4 @@
-function [model_names, wlf, wlf_3mm, wlf_10mm, wake_length, mesh_density, mesh_scaling, decay_to, simulation_time] = extract_all_wlf(root_path, model_sets)
+function [model_names, wlf_1mm, wlf_3mm, wlf_10mm, wake_length, mesh_density, mesh_scaling, n_cores, simulation_time] = extract_all_wlf(root_path, model_sets)
 
 
 for sts = 1:length(model_sets)
@@ -18,19 +18,21 @@ for sts = 1:length(model_sets)
         load(fullfile(current_folder, 'run_inputs'), 'modelling_inputs');
         load(fullfile(current_folder, 'data_from_run_logs.mat'), 'run_logs');
         model_names{sts,ind} = split_str{ind}{end - 2};
-        wlf(sts,ind) = wake_sweep_data.time_domain_data{end}.wake_loss_factor;
+%         wlf(sts,ind) = wake_sweep_data.time_domain_data{end}.wake_loss_factor;
         wlf_bl_sweep = wake_sweep_data.frequency_domain_data{1, end}.extrap_data.beam_sigma_sweep.wlf;
         bl_sweep = wake_sweep_data.frequency_domain_data{1, end}.extrap_data.beam_sigma_sweep.sig_time *3E8;
+        wlf_1mm(sts,ind) = interp1(bl_sweep, wlf_bl_sweep, 1E-3);
         wlf_3mm(sts,ind) = interp1(bl_sweep, wlf_bl_sweep, 3E-3);
         wlf_10mm(sts,ind) = interp1(bl_sweep, wlf_bl_sweep, 10E-3);
         wake_length(sts,ind) = (round(((wake_sweep_data.time_domain_data{1}.timebase(end)) *3e8)*100))/100;
         mesh_density(sts, ind) = modelling_inputs.mesh_stepsize;
         mesh_scaling(sts, ind) = modelling_inputs.mesh_density_scaling;
+        n_cores(sts, ind) = str2num(modelling_inputs.n_cores);
 %         mesh_scaling(sts, ind) = 1;
         pling = max(abs(pp_data.Wake_potential(:,2)));
         % now looking at the last ~10ps of data
         tail = max(abs(pp_data.Wake_potential(end-600:end,2)));
-        decay_to(sts,ind) = mean(abs(tail));
+%         decay_to(sts,ind) = mean(abs(tail));
         simulation_time(sts, ind) = run_logs.wall_time;
         clear pp_data wake_data
     end %for
