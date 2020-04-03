2485dcbda01c414bafcad3d247ee94be772cc0eb
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-04-03 15:05:46 +0100

    refactoring so that file conversions are only done if necessary.

--- Postprocess_models/postprocess_wakes.m
+++ Postprocess_models/postprocess_wakes.m
@@ -33,73 +33,75 @@ for hwa = 1:length(data)
         warning('postprocess_wakes:NotCompleted', 'The postprocessor has not completed properly')
     end
 end
-
-[file_list, path_list] = dir_list_gen('pp_link/wake/', 'ps');
-if  ~isempty(file_list)
-    All_scaled_list = file_list(contains(file_list, 'All_scaled'));
-    if ~isempty(All_scaled_list)
-        for cek = 1:length(All_scaled_list)
-            system(['convert ', [path_list, All_scaled_list{cek}], ' -rotate -90 ', [path_list, All_scaled_list{cek}(1:end-2)],'png']);
-        end
-        system(['ffmpeg -r 2 -f image2 -s 1440x900 -i ', path_list, 'All_scaled_%02d.png -vcodec mpeg4 -pix_fmt yuv420p ' path_list, 'All_scaled.mp4'])
-    end %if
-    All_power_list = file_list(contains(file_list, 'All_power_scaled'));
-    if ~isempty(All_power_list)
-        for cek = 1:length(All_power_list)
-            system(['convert ', [path_list, All_power_list{cek}], ' -rotate -90 ', [path_list, All_power_list{cek}(1:end-2)],'png']);
-        end
-        system(['ffmpeg -r 2 -f image2 -s 1440x900 -i ', path_list, 'All_power_%02d.png -vcodec mpeg4 -pix_fmt yuv420p ' path_list, 'All_power.mp4'])
-    end %if
-end %if
 % find the gld files for the field output images.
 gld_list = dir_list_gen('pp_link/wake/', 'gld');
 if ~isempty(gld_list)
     gld_files = dir_list_gen('temp_scratch', 'gld', 1);
     parfor fjh = 1:length(gld_files)
         system(['gd1.3dplot -colorps -geometry 800x600 -o ',gld_files{fjh}(1:end-3), 'ps -i ' , gld_files{fjh}]);
-        system(['convert ', gld_files{fjh}(1:end-3), 'ps -rotate -90 ',gld_files{fjh}(1:end-3) ,'png']);
-    end %for
-    system('ffmpeg -r 10 -f image2 -s 1920x1080 -i temp_scratch/3D-Arrowplot.%04d.png -vcodec mpeg4 -pix_fmt yuv420p test.mp4')
-    movefile('test.mp4', 'pp_link/wake/h_on_surfaces.mp4')
+    end %parfor
 end %if
-
+[file_list, path_list] = dir_list_gen('pp_link/wake/', 'ps');
+for cek = 1:length(file_list)
+    [stat, ~] = system(['convert ', fullfile(path_list, file_list{cek}), ' -rotate -90 ', [path_list, file_list{cek}(1:end-2)],'png']);
+    if stat == 0
+        rm(fullfile(path_list, file_list{cek}))
+    end %if
+end %for
 gif_list = dir_list_gen('pp_link/wake/', 'gif');
 if ~isempty(gif_list)
     convert_status = system('for file in pp_link/wake/*.gif; do convert $file pp_link/wake/`basename $file .gif`.png; done');
     if convert_status == 0
         [~] = system('rm -f pp_link/wake/*.gif');
     end%if
-    if exist('pp_link/wake/E2DHy000000002.png', 'file') == 2
-        E2DHy_status = system('ffmpeg -r 10 -i pp_link/wake/E2DHy%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/E2Dy.mp4');
-        if E2DHy_status == 0
-            [~] = system('rm -f pp_link/wake/E2DHy*.png');
-        end %if
+end %if
+%% Making Videos
+if  ~isempty(file_list)
+    All_scaled_list = file_list(contains(file_list, 'All_scaled'));
+    if ~isempty(All_scaled_list)
+        system(['ffmpeg -r 2 -f image2 -s 1440x900 -i ', path_list, 'All_scaled_%02d.png -vcodec mpeg4 -pix_fmt yuv420p ' path_list, 'All_scaled.mp4'])
     end %if
-    if exist('pp_link/wake/E2DHx000000002.png', 'file') == 2
-        E2DHx_status = system('ffmpeg -r 10 -i pp_link/wake/E2DHx%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/E2Dx.mp4');
-        if E2DHx_status == 0
-            [~] = system('rm -f pp_link/wake/E2DHx*.png');
-        end %if
-    end %if
-    if exist('pp_link/wake/H2DHy000000002.png', 'file') == 2
-        H2DHy_status = system('ffmpeg -r 10 -i pp_link/wake/H2DHy%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/H2Dy.mp4');
-        if H2DHy_status == 0
-            [~] = system('rm -f pp_link/wake/H2DHy*.png');
-        end %if
-    end %if
-    if exist('pp_link/wake/H2DHx000000002.png', 'file') == 2
-        H2DHx_status = system('ffmpeg -r 10 -i pp_link/wake/H2DHx%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/H2Dx.mp4');
-        if H2DHx_status == 0
-            [~] = system('rm -f pp_link/wake/H2DHx*.png');
-        end %if
-    end %if
-    if exist('pp_link/wake/honmat3D000000002.png', 'file') == 2
-        honmat_status = system('ffmpeg -r 10 -i pp_link/wake/honmat3D%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/Honmat3D.mp4');
-        if honmat_status == 0
-            [~] = system('rm -f pp_link/wake/honmat3D*.png');
-        end %if
+    All_power_list = file_list(contains(file_list, 'All_power_scaled'));
+    if ~isempty(All_power_list)
+        system(['ffmpeg -r 2 -f image2 -s 1440x900 -i ', path_list, 'All_power_%02d.png -vcodec mpeg4 -pix_fmt yuv420p ' path_list, 'All_power.mp4'])
     end %if
 end %if
+if ~isempty(gld_list)
+    system('ffmpeg -r 10 -f image2 -s 1920x1080 -i temp_scratch/3D-Arrowplot.%04d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/h_on_surfaces.mp4')
+end %if
+
+
+if exist('pp_link/wake/E2DHy000000002.png', 'file') == 2
+    E2DHy_status = system('ffmpeg -r 10 -i pp_link/wake/E2DHy%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/E2Dy.mp4');
+    if E2DHy_status == 0
+        [~] = system('rm -f pp_link/wake/E2DHy*.png');
+    end %if
+end %if
+if exist('pp_link/wake/E2DHx000000002.png', 'file') == 2
+    E2DHx_status = system('ffmpeg -r 10 -i pp_link/wake/E2DHx%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/E2Dx.mp4');
+    if E2DHx_status == 0
+        [~] = system('rm -f pp_link/wake/E2DHx*.png');
+    end %if
+end %if
+if exist('pp_link/wake/H2DHy000000002.png', 'file') == 2
+    H2DHy_status = system('ffmpeg -r 10 -i pp_link/wake/H2DHy%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/H2Dy.mp4');
+    if H2DHy_status == 0
+        [~] = system('rm -f pp_link/wake/H2DHy*.png');
+    end %if
+end %if
+if exist('pp_link/wake/H2DHx000000002.png', 'file') == 2
+    H2DHx_status = system('ffmpeg -r 10 -i pp_link/wake/H2DHx%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/H2Dx.mp4');
+    if H2DHx_status == 0
+        [~] = system('rm -f pp_link/wake/H2DHx*.png');
+    end %if
+end %if
+if exist('pp_link/wake/honmat3D000000002.png', 'file') == 2
+    honmat_status = system('ffmpeg -r 10 -i pp_link/wake/honmat3D%9d.png -vcodec mpeg4 -pix_fmt yuv420p pp_link/wake/Honmat3D.mp4');
+    if honmat_status == 0
+        [~] = system('rm -f pp_link/wake/honmat3D*.png');
+    end %if
+end %if
+
 %% find the location of all the required output files
 [ WP_beam, WP_offset, WI_s, WI_x, WI_y, ...
     Port_mat, port_names_table, Energy, Energy_in_ceramics ] =...
