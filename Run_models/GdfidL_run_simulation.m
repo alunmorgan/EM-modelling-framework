b843279cc20c149c5c5d7d9b8168b28463fa2d1d
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-04-29 15:24:48 +0100

    Making the code more resiliant to file system transfer failures.

--- Run_models/GdfidL_run_simulation.m
+++ Run_models/GdfidL_run_simulation.m
@@ -1,4 +1,4 @@
-function GdfidL_run_simulation(sim_type, paths, modelling_inputs, ow_behaviour)
+function output_data_location = GdfidL_run_simulation(sim_type, paths, modelling_inputs, ow_behaviour)
 % Takes the geometry specification, adds the setup for a  simulation and
 % runs the simulation with the desired calculational precision.
 %
@@ -17,66 +17,51 @@ function GdfidL_run_simulation(sim_type, paths, modelling_inputs, ow_behaviour)
 % will kill performance. So initially write to a local drive and then move
 % it.
 
-if strcmpi(sim_type, 'geometry')
-    sim_f_name = 'geometry';
-    sim_name = 'Geometry';
-elseif strcmpi(sim_type, 'wake')
-    sim_f_name = 'wake';
-    sim_name = 'Wake';
-elseif strcmpi(sim_type, 's-parameter') || strcmpi(sim_type, 's_parameter') ||...
-        strcmpi(sim_type, 's-parameters') || strcmpi(sim_type, 's_parameters')
-    sim_f_name = 's_parameters';
-    sim_name = 'S-parameter';
-elseif strcmpi(sim_type, 'eigenmode')
-    sim_f_name = 'eigenmode';
-    sim_name = 'Eigenmode';
-elseif strcmpi(sim_type, 'lossy eigenmode')
-    sim_f_name = 'lossy_eigenmode';
-    sim_name = 'Lossy eigenmode';
-elseif strcmpi(sim_type, 'shunt')
-    sim_f_name = 'shunt';
-    sim_name = 'Shunt';
-end %if
+% 'geometry', 'wake', 's_parameters', 'eigenmode', 'lossy_eigenmode', 'shunt'
+
 skip = strcmp(ow_behaviour, 'skip');
-% Create the required top leveloutput directories.
+% Create the required top level output directories.
 results_storage_location = fullfile(paths.storage_path, modelling_inputs.model_name);
-run_sim = make_data_store(modelling_inputs.model_name, results_storage_location, sim_f_name, skip);
+run_sim = make_data_store(modelling_inputs.model_name, results_storage_location, sim_type, skip);
 
 if run_sim == 1
-%     mkdir(results_storage_location, sim_f_name)
+    %     mkdir(results_storage_location, sim_f_name)
     % Move into the temporary folder.
     [old_loc, tmp_location] = move_into_tempororary_folder(paths.scratch_path);
     % If the simulation type is S-paramter then you need a simulation for
     % each excited port. For Shunt you need a simulation for each frequency.
     % For the other types you just need a single simulation.
     f_range = 1.3E9:5E7:1.9E9; % FIXME This needs to become a parameter
-    if strcmp(sim_name, 'S-parameter')
+    if strcmp(sim_type, 's_parameter')
         n_cycles = length(modelling_inputs.ports);
-    elseif strcmp(sim_name, 'Shunt')
+    elseif strcmp(sim_type, 'shunt')
         n_cycles = length(f_range);
     else
         n_cycles = 1;
     end %if
-    
+    output_data_location = cell(1,n_cycles);
     for nes = 1:n_cycles
         temp_files('make')
-        
-        %         modify_mesh_definition(paths.storage_path, 'temp_data', modelling_inputs.geometry_fraction)
         frequency = num2str(f_range(nes));
         if isempty(modelling_inputs.ports)
             port_name = 'NULL';
         else
             port_name = modelling_inputs.ports{nes};
         end %if
-        arch_out = construct_storage_area_path(results_storage_location, sim_f_name, port_name, frequency);
-        construct_gdf_file(sim_name, modelling_inputs, port_name, frequency)
-        disp(['Running ', sim_name,' simulation for ', modelling_inputs.model_name, '.'])
+        arch_out = construct_storage_area_path(results_storage_location, sim_type, port_name, frequency);
+        construct_gdf_file(sim_type, modelling_inputs, port_name, frequency)
+        disp(['Running ', sim_type,' simulation for ', modelling_inputs.model_name, '.'])
         GdfidL_simulation_core(modelling_inputs.version, modelling_inputs.precision)
-        save(fullfile(results_storage_location, sim_f_name, 'run_inputs.mat'), 'paths', 'modelling_inputs')
-        movefile('temp_data/*', arch_out);
-        %         copyfile(path_to_model_file, arch_out);
-        temp_files('remove')
+        save(fullfile('temp_data', 'run_inputs.mat'), 'paths', 'modelling_inputs')
+        [status, message] = movefile('temp_data', arch_out);
+        if status == 1
+            temp_files('remove')
+            output_data_location{1, nes} = arch_out;
+        elseif status == 0
+            disp(['Error in file transfer - data left in ', tmp_location])
+            disp(message)
+            output_data_location{1, nes} = tmp_location;
+        end %if
     end %for
     cd(old_loc)
-    rmdir(tmp_location,'s');
-end
+end %if
--- Run_models/run_models.m
+++ Run_models/run_models.m
@@ -29,10 +29,10 @@ modelling_inputs = run_inputs_setup_STL(mi);
 
 % Running the different simulators for each model.
 for awh = 1:length(modelling_inputs)
-    % Making the directory to store the run data in.
-    if ~exist(fullfile(mi.paths.storage_path, modelling_inputs{awh}.model_name),'file')
-        mkdir(fullfile(mi.paths.storage_path, modelling_inputs{awh}.model_name))
-    end %if
+%     % Making the directory to store the run data in.
+%     if ~exist(fullfile(mi.paths.storage_path, modelling_inputs{awh}.model_name),'file')
+%         mkdir(fullfile(mi.paths.storage_path, modelling_inputs{awh}.model_name))
+%     end %if
     %     ow_behaviour = '';
     % Write update to the command line
     disp(datestr(now))
@@ -41,7 +41,7 @@ for awh = 1:length(modelling_inputs)
     
     if ~isempty(strfind(mi.simulation_defs.sim_select, 'g'))
         try
-            GdfidL_run_simulation('geometry', mi.paths, modelling_inputs{awh}, ...
+           simulation_result_locations =  GdfidL_run_simulation('geometry', mi.paths, modelling_inputs{awh}, ...
                 force_sim);
         catch ERR
             display_modelling_error(ERR, 'geometry')
@@ -49,7 +49,7 @@ for awh = 1:length(modelling_inputs)
     end %if
     if~isempty(strfind(mi.simulation_defs.sim_select, 'w'))
         try
-            GdfidL_run_simulation('wake', mi.paths, modelling_inputs{awh}, ...
+            simulation_result_locations = GdfidL_run_simulation('wake', mi.paths, modelling_inputs{awh}, ...
                 force_sim);
         catch ERR
             display_modelling_error(ERR, 'wake')
@@ -63,7 +63,7 @@ for awh = 1:length(modelling_inputs)
     end %if
     if ~isempty(strfind(mi.simulation_defs.sim_select, 's'))
         try
-            GdfidL_run_simulation('s-parameter', mi.paths, modelling_inputs{awh}, ...
+            simulation_result_locations = GdfidL_run_simulation('s-parameter', mi.paths, modelling_inputs{awh}, ...
                 force_sim);
         catch ERR
             display_modelling_error(ERR, 'S-parameter')
@@ -71,7 +71,7 @@ for awh = 1:length(modelling_inputs)
     end %if
     if ~isempty(strfind(mi.simulation_defs.sim_select, 'e'))
         try
-            GdfidL_run_simulation('eigenmode', mi.paths, modelling_inputs{awh}, ...
+           simulation_result_locations = GdfidL_run_simulation('eigenmode', mi.paths, modelling_inputs{awh}, ...
                 force_sim);
         catch ERR
             display_modelling_error(ERR, 'eigenmode')
@@ -85,7 +85,7 @@ for awh = 1:length(modelling_inputs)
     end %if
     if ~isempty(strfind(mi.simulation_defs.sim_select, 'l'))
         try
-            GdfidL_run_simulation('lossy eigenmode', mi.paths, modelling_inputs{awh}, ...
+            simulation_result_locations = GdfidL_run_simulation('lossy eigenmode', mi.paths, modelling_inputs{awh}, ...
                 force_sim);
         catch ERR
             display_modelling_error(ERR, 'lossy eigenmode')
@@ -93,7 +93,7 @@ for awh = 1:length(modelling_inputs)
     end %if
     if ~isempty(strfind(mi.simulation_defs.sim_select, 'r'))
         try
-            GdfidL_run_simulation('shunt', mi.paths, modelling_inputs{awh}, ...
+            simulation_result_locations = GdfidL_run_simulation('shunt', mi.paths, modelling_inputs{awh}, ...
                 force_sim);
         catch ERR
             display_modelling_error(ERR, 'shunt')
