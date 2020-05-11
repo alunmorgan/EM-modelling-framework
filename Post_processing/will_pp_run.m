2de37677c1088f20da78e254bf1c431c73e25dcb
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-05-11 10:57:45 +0100

    Changing the run criteria to be the absence of the last file written by the post processing code rather than the presence of teh port processing folder. This means that if the process is interuppeted before completion it will automatically run again the next time the postprocessing code is run.

--- Post_processing/will_pp_run.m
+++ Post_processing/will_pp_run.m
@@ -1,9 +1,9 @@
 function run_pp = will_pp_run(sim_type, skip_setting)
 
 % works for wake and eigenmode.
-if strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type), 'dir') == 7
+if strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 7
     run_pp = 0;
-elseif strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type), 'dir') == 0
+elseif strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 0
     run_pp = 1;
 elseif strcmp(skip_setting, 'no_skip')
     run_pp = 1;
