b80880731ce13e71047a46bd37db8b513e6a4d5f
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-05-11 11:27:56 +0100

    fixing which file type to look for.

--- Post_processing/will_pp_run.m
+++ Post_processing/will_pp_run.m
@@ -1,7 +1,7 @@
 function run_pp = will_pp_run(sim_type, skip_setting)
 
 % works for wake and eigenmode.
-if strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 7
+if strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 2
     run_pp = 0;
 elseif strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 0
     run_pp = 1;
