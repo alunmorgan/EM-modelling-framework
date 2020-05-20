cf942d1680c70c0d4d66fddf62b4da85a855e6cc
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-05-20 14:52:26 +0100

    Changing to just use the wake loss factor for the set length for now.

--- Analysis/get_wlf.m
+++ Analysis/get_wlf.m
@@ -1,19 +1,24 @@
-function get_wlf(root, model_sets)
+function get_wlf(model_sets)
 
-[model_names, wlf_1mm, wlf_3mm, wlf_10mm, wake_length_t, mesh_stepsize, mesh_scaling, n_cores, simulation_time] = extract_all_wlf(root, model_sets);
-wlf_1mm = wlf_1mm .* 1E-9;
-    wlf_3mm = wlf_3mm .* 1E-9;
-    wlf_10mm = wlf_10mm .*1E-9;
+
+[root, ~, ~] = analysis_model_settings_library;
+[model_names, wlf, wake_length_t, mesh_stepsize, mesh_scaling, n_cores, simulation_time] = extract_all_wlf(root, model_sets);
+wlf = wlf .* 1E-9;
+% wlf_1mm = wlf_1mm .* 1E-9;
+%     wlf_3mm = wlf_3mm .* 1E-9;
+%     wlf_10mm = wlf_10mm .*1E-9;
      simulation_time = simulation_time ./ 3600;
     mesh_stepsize = mesh_stepsize ./ mesh_scaling .* 1e6;
 for kfn = 1:size(model_names,1)
-    T = table(model_names(kfn, :)', wlf_1mm(kfn,:)', wlf_3mm(kfn,:)', ...
-        wlf_10mm(kfn,:)', wake_length_t(kfn,:)', mesh_stepsize(kfn,:)', ...
+%     T = table(model_names(kfn, :)', wlf_1mm(kfn,:)', wlf_3mm(kfn,:)', ...
+%         wlf_10mm(kfn,:)', wake_length_t(kfn,:)', mesh_stepsize(kfn,:)', ...
+%         simulation_time(kfn,:)', n_cores(kfn,:)');
+T = table(model_names(kfn, :)', wlf(kfn,:)', wake_length_t(kfn,:)', mesh_stepsize(kfn,:)', ...
         simulation_time(kfn,:)', n_cores(kfn,:)');
     disp(T)
 end %for
 
-bar(n_cores, simulation_time ./ min(simulation_time))
-title('Finding optimum core selection')
-xlabel('Number of cores')
-ylabel('Multiple of minimum runtime')
+% bar(n_cores, simulation_time ./ min(simulation_time))
+% title('Finding optimum core selection')
+% xlabel('Number of cores')
+% ylabel('Multiple of minimum runtime')
