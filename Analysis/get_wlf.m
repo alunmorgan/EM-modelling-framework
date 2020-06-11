bc983e2f03078adb39ab4ad132667cbe5878a803
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-06-11 09:36:31 +0100

    Improved the output of the final results table.

--- Analysis/get_wlf.m
+++ Analysis/get_wlf.m
@@ -1,24 +1,53 @@
-function get_wlf(model_sets)
+function out = get_wlf(model_sets)
 
 
 [root, ~, ~] = analysis_model_settings_library;
-[model_names, wlf, wake_length_t, mesh_stepsize, mesh_scaling, n_cores, simulation_time] = extract_all_wlf(root, model_sets);
-wlf = wlf .* 1E-9;
-% wlf_1mm = wlf_1mm .* 1E-9;
-%     wlf_3mm = wlf_3mm .* 1E-9;
-%     wlf_10mm = wlf_10mm .*1E-9;
-     simulation_time = simulation_time ./ 3600;
-    mesh_stepsize = mesh_stepsize ./ mesh_scaling .* 1e6;
-for kfn = 1:size(model_names,1)
-%     T = table(model_names(kfn, :)', wlf_1mm(kfn,:)', wlf_3mm(kfn,:)', ...
-%         wlf_10mm(kfn,:)', wake_length_t(kfn,:)', mesh_stepsize(kfn,:)', ...
-%         simulation_time(kfn,:)', n_cores(kfn,:)');
-T = table(model_names(kfn, :)', wlf(kfn,:)', wake_length_t(kfn,:)', mesh_stepsize(kfn,:)', ...
-        simulation_time(kfn,:)', n_cores(kfn,:)');
+[extracted_data] = extract_all_wlf(root, model_sets);
+wlf = round(extracted_data.wlf .* 1E-9);
+sim_time_string = sec_to_time_string(extracted_data.simulation_time);
+mesh_stepsize = extracted_data.mesh_density ./ extracted_data.mesh_scaling .* 1e6;
+timestep = round(extracted_data.timestep .* 1E15);
+number_of_cells = round(extracted_data.number_of_cells ./ 1E6);
+for kfn = 1:size(extracted_data.model_names,1)
+    varnames = {'simulation time', 'number of mesh cells (million)', ...
+        'memory used (MB)', 'timestep (fs)', ...
+        'wlf (mV/pC)', 'wake length (m)', ...
+        'mesh stepsize (um)', 'number of cores'};
+    T = table(sim_time_string(kfn,:)', number_of_cells(kfn,:)', ...
+        extracted_data.memory_usage(kfn,:)', timestep(kfn,:)',...
+        wlf(kfn,:)', extracted_data.wake_length(kfn,:)', ...
+        mesh_stepsize(kfn,:)',...
+        extracted_data.n_cores(kfn,:)',...
+        'RowNames', extracted_data.model_names(kfn, :)', 'VariableNames', varnames);
     disp(T)
+    out.(model_sets{kfn}).wlf_table = T;
 end %for
 
-% bar(n_cores, simulation_time ./ min(simulation_time))
-% title('Finding optimum core selection')
-% xlabel('Number of cores')
-% ylabel('Multiple of minimum runtime')
+end %function
+
+function sim_time_string = sec_to_time_string(simulation_time)
+
+st_days = floor(simulation_time ./ 3600 ./24);
+st_hours = floor(simulation_time ./ 3600 - st_days .* 24);
+st_mins = floor(simulation_time ./ 60 - st_hours .* 60);
+for kef = 1:length(simulation_time)
+    if st_days(kef) ~= 0
+        day_string = [num2str(st_days(kef)), ' days '];
+    else
+        day_string = '';
+    end %if
+    if st_hours(kef) ~= 0
+        hour_string = [num2str(st_hours(kef)), ' hours '];
+    else
+        hour_string = '';
+    end %if
+    if st_mins(kef) ~= 0
+        min_string = [num2str(st_mins(kef)), ' mins '];
+    else
+        min_string = '';
+    end %if
+    sim_time_string{kef} = [day_string, hour_string, min_string];
+end %for
+ sim_time_string = string(sim_time_string);
+
+end %function
