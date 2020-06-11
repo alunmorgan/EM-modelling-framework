92faf240eb1c5c5170b6356aa9243fbfd8873ca4
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-06-11 09:42:14 +0100

    Fixing a logic error.

--- Report_generation/generate_report.m
+++ Report_generation/generate_report.m
@@ -1,6 +1,6 @@
 function generate_report(root_path, ppi, input_settings, override)
 
-    datasets = find_datasets(root_path, input_settings);
+    datasets = find_datasets(fullfile(root_path, input_settings{1}));
 for ind = 1:length(datasets)
 %     model_name = split_str{ind}{end - 2};
 %     path_to_data = fullfile(split_str{ind}{1:end-2});
@@ -8,7 +8,7 @@ for ind = 1:length(datasets)
 %         % This will ensure the leading slash is kept.
 %         path_to_data = [filesep, path_to_data];
 %     end %if
-    if override == 1 || exist(fullfile(datasets{ind}.path_to_data, 'Report.pdf')) == 0
+    if override == 1 || ~exist(fullfile(datasets{ind}.path_to_data, 'Report.pdf'), 'file') == 0
         disp(['Generating a report for ', datasets{ind}.model_name])
 %         generate_graphs(path_to_data, ppi, input_settings{4})
         Report_setup(datasets{ind}.path_to_data, ppi, input_settings{2}, input_settings{4})
