4f6dd039fee1471eae7e60fac969bab38de1dd69
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-08-07 09:27:08 +0100

    Separated results into separate tables.

--- Analysis/get_wlf.m
+++ Analysis/get_wlf.m
@@ -9,31 +9,80 @@ mesh_stepsize = extracted_data.mesh_density ./ extracted_data.mesh_scaling .* 1e
 timestep = round(extracted_data.timestep .* 1E15);
 number_of_cells = round(extracted_data.number_of_cells ./ 1E6);
 for kfn = 1:size(extracted_data.model_names,1)
-    varnames = {'simulation time', 'number of mesh cells (million)', ...
+    varnames_setup = {'simulation time', 'number of mesh cells (million)', ...
         'memory used (MB)', 'timestep (fs)', ...
-        'wlf (mV/pC)', 'wake length (m)', ...
-        'mesh stepsize (um)', 'number of cores', ...
+        'mesh stepsize (um)', 'number of cores'};
+    T_setup = table(sim_time_string(kfn,:)', number_of_cells(kfn,:)', ...
+        extracted_data.memory_usage(kfn,:)', timestep(kfn,:)',...
+        mesh_stepsize(kfn,:)', extracted_data.n_cores(kfn,:)',...
+        'RowNames', extracted_data.model_names(kfn, :)', 'VariableNames', varnames_setup);
+    out.(model_sets{kfn}).setup_table = T_setup;
+    disp(T_setup(:,1:3))
+    disp(T_setup(:,4:6))
+    
+    varnames_wake = {'wlf (mV/pC)', 'wake length (m)'};
+    T_wake = table(...
+        wlf(kfn,:)', extracted_data.wake_length(kfn,:)', ...
+        'RowNames', extracted_data.model_names(kfn, :)', 'VariableNames', varnames_wake);
+    out.(model_sets{kfn}).wake_table = T_wake;
+    disp(T_wake(:,:))
+    
+    varnames_losses = {...
         'beam port loss [f](%)', 'signal port loss [f](%)', 'structure loss [f](%)',...
         'beam port loss [t](%)', 'signal port loss [t](%)', 'structure loss [t](%)'};
-    T = table(sim_time_string(kfn,:)', number_of_cells(kfn,:)', ...
-        extracted_data.memory_usage(kfn,:)', timestep(kfn,:)',...
-        wlf(kfn,:)', extracted_data.wake_length(kfn,:)', ...
-        mesh_stepsize(kfn,:)',...
-        extracted_data.n_cores(kfn,:)',...
+    T_losses = table(...
         extracted_data.fractional_loss_beam_ports(kfn,:)' .* 100,...
         extracted_data.fractional_loss_signal_ports(kfn,:)' .* 100,...
         extracted_data.fractional_loss_structure(kfn,:)' .* 100,...
         extracted_data.beam_port_loss(kfn,:)' .* 100,...
         extracted_data.signal_port_loss(kfn,:)' .* 100,...
         extracted_data.structure_loss(kfn,:)' .* 100,...
-        'RowNames', extracted_data.model_names(kfn, :)', 'VariableNames', varnames);
-    disp(T(:,1:4))
-    disp(T(:,5:8))
-    disp(T(:,9:11))
-    disp(T(:,12:14))
-    out.(model_sets{kfn}).wlf_table = T;
+        'RowNames', extracted_data.model_names(kfn, :)', 'VariableNames', varnames_losses);
+    out.(model_sets{kfn}).losses_table = T_losses;
+    disp(T_losses(:,1:3))
+    disp(T_losses(:,4:6))
+    
+    if length(extracted_data.port{kfn}.dominant_signal_amplitudes) > 2
+        varnames_port_signals = {};
+        ports = {};
+        for wha = 1:size(extracted_data.port,2) %for each model in set
+             if wha == 1
+                 for hne = 3:length(extracted_data.port{kfn,wha}.dominant_signal_amplitudes) %for each signal port
+                varnames_port_signals = cat(2, varnames_port_signals, [extracted_data.port{kfn,wha}.labels{hne}, ' amplitude'], ...
+                    [extracted_data.port{kfn, wha}.labels{hne}, ' dominant mode']);
+                ports = cat(2, ports, extracted_data.port{kfn,wha}.labels{hne}, ...
+                    extracted_data.port{kfn, wha}.labels{hne});
+                 end %for
+                end %if
+            for hne = 3:length(extracted_data.port{kfn,wha}.dominant_signal_amplitudes) %for each signal port
+                label = extracted_data.port{kfn,wha}.labels{hne};
+                label_inds = find(strcmp(ports, label));
+                if isempty(label_inds)
+                    label_inds = find(strcmp(ports, label(1:end-2)));
+                    if isempty(label_inds)
+                        % added new port to output - The only time this should
+                        % happen is if the base model has reduced geometry and
+                        % some of the others do not.
+                        varnames_port_signals = cat(2, varnames_port_signals, [label, ' amplitude'], ...
+                            [label, ' dominant mode']);
+                        label_inds = [length(varnames_port_signals) -1, length(varnames_port_signals)];
+                    else
+                        %it is a duplicated port - so can be ignored for this.
+                        continue
+                    end %if
+                end %if
+                port_signals_table_data{label_inds(1)}(wha,1) = extracted_data.port{kfn,wha}.dominant_signal_amplitudes(hne);
+                port_signals_table_data{label_inds(2)}(wha,1) = extracted_data.port{kfn,wha}.dominant_modes(hne);
+            end %for
+        end %for
+        T_port_signals = table(port_signals_table_data{:},...
+            'RowNames', extracted_data.model_names(kfn, :)', 'VariableNames', varnames_port_signals);
+        out.(model_sets{kfn}).port_signals_table = T_port_signals;
+    else
+        out.(model_sets{kfn}).port_signals_table = NaN;
+    end %if
+    clear varnames_setup varnames_wake varnames_losses varnames_port_signals T_setup T_wake T_losses T_port_signals
 end %for
-
 end %function
 
 function sim_time_string = sec_to_time_string(simulation_time)
@@ -59,6 +108,6 @@ for kef = 1:length(simulation_time)
     end %if
     sim_time_string{kef} = [day_string, hour_string, min_string];
 end %for
- sim_time_string = string(sim_time_string);
+sim_time_string = string(sim_time_string);
 
 end %function
