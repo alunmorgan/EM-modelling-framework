bb4c0b397050d47e95c388ab522d328c52706490
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-08-10 10:23:07 +0100

    Removing unneeded code.

--- Analysis/time_domain_analysis.m
+++ Analysis/time_domain_analysis.m
@@ -11,14 +11,8 @@ wake_potential_trans_quad_y = raw_data.time_series_data.Wake_potential_trans_qua
 wake_potential_trans_dipole_x = raw_data.time_series_data.Wake_potential_trans_dipole_X ./ log.charge; 
 wake_potential_trans_dipole_y = raw_data.time_series_data.Wake_potential_trans_dipole_Y ./ log.charge; 
 
-% % Rescale the charge distribution timebase to match the wake potential scale
-% ncd_scaled = interp1(raw_data.Charge_distribution(:,1), raw_data.Charge_distribution(:,2),...
-%     raw_data.Wake_potential(:,1));
-% % replace any NaNs with zeros. The interpolation function will have returned
-% % NaNs where the new timescale was longer than the original one.
-% ncd_scaled(isnan(ncd_scaled)) = 0;
-time_step = abs(raw_data.time_series_data.timescale_common(2) - raw_data.time_series_data.timescale_common(1));
 % Scale the charge distribution to have the normalised integral value of 1C.
+time_step = abs(raw_data.time_series_data.timescale_common(2) - raw_data.time_series_data.timescale_common(1));
 cd_scaling = sum(raw_data.time_series_data.Charge_distribution .* time_step, 'omitnan');
 charge_distribution = raw_data.time_series_data.Charge_distribution ./ cd_scaling; 
 
@@ -26,7 +20,7 @@ charge_distribution = raw_data.time_series_data.Charge_distribution ./ cd_scalin
 wake_loss_dist = charge_distribution .* wake_potential; %(V\C)
 
 % Calculate the wake loss factor (V\C)
-wake_loss_factor = -sum(wake_loss_dist .* time_step, 'omitnan'); %(V\C)
+wake_loss_factor = -sum(wake_loss_dist, 'omitnan') .* time_step; %(V\C)
 
 % Calculate the energy lost from the beam (J)
 loss_from_beam = wake_loss_factor * log.charge.^2 ;
