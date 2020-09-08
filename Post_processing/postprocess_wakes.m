38aded286ffcbe5d60c0936483d3f1d469efb1da
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-09-08 17:35:13 +0100

    update to postprocessing setup code.

--- Post_processing/postprocess_wakes.m
+++ Post_processing/postprocess_wakes.m
@@ -1,4 +1,4 @@
-function raw_data = postprocess_wakes(modelling_inputs, log)
+function data = postprocess_wakes(modelling_inputs, log)
 % Runs the GdfidL postprocessor on the selected data.
 % The model data has already been selected using soft links.
 %
@@ -18,6 +18,6 @@ wake_output_directory = fullfile('pp_link', 'wake');
 postprocess_core(wake_output_directory, modelling_inputs.version, 'wake', 0, 0);
 %% Extract the wake data
 output_file_locations = GdfidL_find_ouput(wake_output_directory);
-raw_data = extract_wake_data_from_pp_output_files(output_file_locations, log, modelling_inputs, tstart);
+data = extract_wake_data_from_pp_output_files(output_file_locations, log, modelling_inputs, tstart);
 % raw_data.port.t_start = tstart;
 
