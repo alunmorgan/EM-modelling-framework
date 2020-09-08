05e7d750d1b92b60598d9b8a2da29b18475ab05a
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-09-08 17:21:38 +0100

    updating blending code to cope with geometry fraction changes.

--- Blending_results/Blend_reports.m
+++ Blending_results/Blend_reports.m
@@ -22,10 +22,31 @@ for jse = 1:length(sets)
         report_input.output_loc = fullfile(results_loc, report_input.rep_title);
         
         names_in_sweep = names(contains(names, '_Base') | contains(names, sweeps{ewh}));
-        good_data = ones(length(names_in_sweep),1);
         for psw = length(names_in_sweep):-1:1
-            [param_names(psw,:), param_vals(psw,:), good_data(psw), modelling_inputs] = ...
+            [param_names_temp, param_vals_temp, good_data(psw), modelling_inputs{psw}] = ...
                 params_in_simulation(fullfile(results_loc, names_in_sweep{psw}));
+            if good_data(psw) == 0
+                for shf = 1:size(param_names,2)
+                    param_names{psw,shf} = NaN;
+                end %for
+            else
+                param_names(psw,:) = param_names_temp;
+            end %if
+            if good_data(psw) == 0
+                for shf = 1:size(param_vals,2)
+                    param_vals{psw,shf} = NaN;
+                end %for
+            else
+                param_vals(psw,:) = param_vals_temp;
+            end %if
+            clear param_names_temp param_vals_temp,
+            % add some values from the input file which do not show in the
+            % postprocessing log.
+            if isfield(modelling_inputs{psw}, 'port_multiple')
+                report_input.port_multiple{psw} = modelling_inputs{psw}.port_multiple;
+                report_input.port_fill_factor{psw} = modelling_inputs{psw}.port_fill_factor;
+                report_input.volume_fill_factor{psw} = modelling_inputs{psw}.volume_fill_factor;
+            end %if
         end %for
         if sum(good_data) ==0
             warning('No valid data. Skipping report generation')
@@ -35,6 +56,9 @@ for jse = 1:length(sets)
         param_names = param_names(good_data == 1,:);
         param_vals = param_vals(good_data == 1,:);
         names_in_sweep = names_in_sweep(good_data == 1,:);
+        report_input.port_multiple = report_input.port_multiple(good_data == 1);
+        report_input.port_fill_factor = report_input.port_fill_factor(good_data == 1);
+        report_input.volume_fill_factor = report_input.volume_fill_factor(good_data == 1);
         
         % Identify which parameters vary.
         for sha = size(param_vals,2):-1:1
@@ -50,7 +74,7 @@ for jse = 1:length(sets)
         end %if
         
         report_input.sources = names_in_sweep;
-        report_input.author = modelling_inputs.author;
+        report_input.author = modelling_inputs{1}.author;
         report_input.date = datestr(now,'dd/mm/yyyy');
         report_input.source_path = results_loc;
         report_input.param_names_common = param_names(1, stable);
@@ -58,19 +82,12 @@ for jse = 1:length(sets)
         report_input.swept_name = param_names(1, varying_pars_ind);
         report_input.swept_vals = param_vals(:,varying_pars_ind);
         
-        % add some values from the input file which do not show in the
-        % postprocessing log.
-        if isfield(modelling_inputs, 'port_multiple')
-            report_input.port_multiple = modelling_inputs.port_multiple;
-            report_input.port_fill_factor = modelling_inputs.port_fill_factor;
-            report_input.volume_fill_factor = modelling_inputs.volume_fill_factor;
-        end %if
         if ~exist(report_input.output_loc, 'dir')
             mkdir(report_input.output_loc)
         end %if
-        s_parameter_extract_single_frequency_data(report_input, [0, 125E6, 250E6, 500E6]);
+        s_parameter_extract_single_frequency_data(report_input);
         Blend_figs(report_input, 'wake', chosen_wake_lengths{jse}, frequency_display_limit);
         Blend_single_report(report_input, chosen_wake_lengths{jse})
-        clear param_names param_vals
+        clear param_names param_vals good_data names_in_sweep report_input
     end %for
 end %for
