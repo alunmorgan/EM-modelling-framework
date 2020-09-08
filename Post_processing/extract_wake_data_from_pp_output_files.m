34c334aabb54c63782cc9875ccb65ee719bc3651
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-09-08 17:32:32 +0100

    moving alpha, and beta data to another part of teh data structure as they vary with some input variations now.

--- Post_processing/extract_wake_data_from_pp_output_files.m
+++ Post_processing/extract_wake_data_from_pp_output_files.m
@@ -58,17 +58,9 @@ end %if
 %% Ports
 if ~iscell(output_file_locations.Port_mat)
     warning('postprocess_wakes:No ports to analyse')
-    port_names = NaN;
     port_data = NaN;
-    alpha = NaN;
-    beta = NaN;
-    cutoff = NaN;
 else
-    [port_timebase, alpha, beta, port_data, cutoff] = read_port_datafiles(output_file_locations.Port_mat, log, ...
-        modelling_inputs.port_fill_factor);
-    [port_names,alpha, beta, port_data, cutoff, tstart] = duplicate_ports(...
-        modelling_inputs.port_multiple, output_file_locations.Port_mat,...
-        modelling_inputs.port_names, alpha, beta, port_data, cutoff, tstart);
+    [port_timebase, port_data] = read_port_datafiles(output_file_locations.Port_mat);
 end
 
 %% Wake potentials
@@ -172,13 +164,8 @@ else
     raw_data.Wake_impedance_trans_dipole_Y = NaN(length(wil_data.data),2);
 end
 raw_data.port.timebase = port_timebase;
-% raw_data.port.data_all = port_data_all;
 raw_data.port.data = port_data;
-raw_data.port.labels = port_names;
-raw_data.port.labels_table = port_names;
-raw_data.port.frequency_cutoffs = cutoff;
-raw_data.port.alpha = alpha;
-raw_data.port.beta = beta;
+raw_data.port.labels = modelling_inputs.port_names;
 raw_data.port.t_start = tstart;
 raw_data.wake_setup.Wake_length = wpl_data.data(end,1) .* 2.99792458E8;
 if isfield(log, 'mat_losses')
