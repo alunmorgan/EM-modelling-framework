62ced3186ecc0442bf800e6e015a6640bf32505f
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-10-12 17:13:28 +0100

    Whitespace fixes to improve readability.

--- Run_models/create_geometry_plots.m
+++ Run_models/create_geometry_plots.m
@@ -48,19 +48,19 @@ model_file = cat(1, model_file, 'showlines=no');
 for ndw = 1:size(modelling_inputs.cuts,1)
     if strcmp(modelling_inputs.cuts{ndw, 1},'x')
         model_file = reset_bounding_box(model_file);
-        model_file = cat(1, model_file, ['   bbxlow=',modelling_inputs.cuts{ndw, 2}]);
-        model_file = cat(1, model_file, ['   bbxhigh=',modelling_inputs.cuts{ndw, 2}]);
+        model_file = cat(1, model_file, ['   bbxlow=', modelling_inputs.cuts{ndw, 2}]);
+        model_file = cat(1, model_file, ['   bbxhigh=', modelling_inputs.cuts{ndw, 2}]);
         model_file = cat(1, model_file, eyepos.twodxeyepos);
         x_count = x_count +1;
-        model_file = cat(1, model_file, ['   plotopts = -o temp_data/2Dmodel_cut_',modelling_inputs.cuts{ndw, 1},'_user',num2str(x_count),'.ps -colorps']);
+        model_file = cat(1, model_file, ['   plotopts = -o temp_data/2Dmodel_cut_', modelling_inputs.cuts{ndw, 1},'_user',num2str(x_count),'.ps -colorps']);
         model_file = cat(1, model_file, 'doit');
     elseif strcmp(modelling_inputs.cuts{ndw, 1},'y')
         model_file = reset_bounding_box(model_file);
-        model_file = cat(1, model_file, ['   bbylow=',modelling_inputs.cuts{ndw, 2}]);
-        model_file = cat(1, model_file, ['   bbyhigh=',modelling_inputs.cuts{ndw, 2}]);
+        model_file = cat(1, model_file, ['   bbylow=', modelling_inputs.cuts{ndw, 2}]);
+        model_file = cat(1, model_file, ['   bbyhigh=', modelling_inputs.cuts{ndw, 2}]);
         model_file = cat(1, model_file, eyepos.twodyeyepos);
         y_count = y_count +1;
-        model_file = cat(1, model_file, ['   plotopts = -o temp_data/2Dmodel_cut_',modelling_inputs.cuts{ndw, 1},'_user', num2str(y_count),'.ps -colorps']);
+        model_file = cat(1, model_file, ['   plotopts = -o temp_data/2Dmodel_cut_', modelling_inputs.cuts{ndw, 1},'_user', num2str(y_count),'.ps -colorps']);
         model_file = cat(1, model_file, 'doit');
     elseif strcmp(modelling_inputs.cuts{ndw, 1},'z')
         model_file = reset_bounding_box(model_file);
