1dc185582e1418a748c54107b7bb325f45773c97
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-11-27 11:59:28 +0000

    One can now select a particular type of graph to be generated (same behaviour as the postprocessing).

--- plotting/plot_models_sets.m
+++ plotting/plot_models_sets.m
@@ -3,6 +3,7 @@ function plot_models_sets(model_sets, varargin)
 p = inputParser;
    addRequired(p,'model_sets',@iscell);
    addOptional(p,'skip_plotting','skip',@ischar);
+   addOptional(p,'plotting_types',{'all'},@iscell);
 parse(p, model_sets, varargin{:});
 
 if strcmp(p.Results.skip_plotting, 'skip')
@@ -17,5 +18,5 @@ ppi = analysis_settings;
 for nw = 1:length(model_sets)
       mse = strcmp(names, model_sets{nw}) == 1;
     input_settings = analysis_library{mse};
-    plot_model(results_loc, ppi, input_settings, override);
+    plot_model(results_loc, ppi, input_settings, override, p.Results.plotting_types);
 end %for
