e2b471e9fb80cf4c43acc9d17224bb77b11e13df
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-11-27 11:57:15 +0000

    Adjusted to lower cutoff to -120.

--- plotting/GdfidL_plot_s_parameters.m
+++ plotting/GdfidL_plot_s_parameters.m
@@ -8,14 +8,14 @@ load(fullfile(fullfile(pth,'data_postprocessed.mat')), 'pp_data'); % contains pp
 
 lines = {'--',':','-.','-'};
 cols_sep = {'y','k','r','b','g','c','m'};
-lower_cutoff = -80;
+lower_cutoff = -120;
 linewidth = 2;
 
 plot_s_param_graph(pp_data, cols_sep, fig_pos, pth, lower_cutoff, linewidth)
 
 if size(pp_data.all_ports,1) > 3
     % Only generate the transmission graphs if there is more than 1 signal port.
-    plot_s_param_transmission_graphs_modes(pp_data, cols_sep, lines, fig_pos, pth, lower_cutoff, linewidth) % START HERE
+    plot_s_param_transmission_graphs_modes(pp_data, cols_sep, lines, fig_pos, pth, lower_cutoff, linewidth)
 end %if
 
 if size(pp_data.all_ports,1) > 3
