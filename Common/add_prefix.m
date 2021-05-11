2cc76e672ea355b28f045113415cd36a37d0ec1a
Alun Morgan <alun.morgan@diamond.ac.uk>
2021-05-11 09:32:38 +0100

    New helper function to give the output files more detailed names. Useful if putting the results on wikis etc.

---
+++ Common/add_prefix.m
@@ -0,0 +1,13 @@
+function add_prefix(root_path, filetype, prefix)
+% Scans the directory for files of the selected type. 
+% Renames them to add the prefix.
+%
+% Example: add_prefix(root_path)
+
+fullPaths = dir_list_gen(root_path, filetype, 1);
+
+for je = 1:length(fullPaths)
+    [a,b,c] = fileparts(fullPaths{je});
+movefile(fullPaths{je}, fullfile(a,[prefix, '_', b, c]));
+clear a b c 
+end %for
