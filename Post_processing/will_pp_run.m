85ad0a4525e50d9a45e3ff649ce6767f2a5361e8
Alun Morgan <alun.morgan@diamond.ac.uk>
2021-05-11 09:44:25 +0100

    Updating logic to follow changes in file naming.

--- Post_processing/will_pp_run.m
+++ Post_processing/will_pp_run.m
@@ -1,12 +1,14 @@
 function run_pp = will_pp_run(sim_type, skip_setting)
 
-% works for wake and eigenmode.
-if strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 2
-    run_pp = 0;
-elseif strcmp(skip_setting, 'skip') && exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 0
+if strcmp(skip_setting, 'no_skip')
     run_pp = 1;
-elseif strcmp(skip_setting, 'no_skip')
-    run_pp = 1;
+elseif strcmp(skip_setting, 'skip')
+    if exist(fullfile('pp_link', sim_type, ['model_', sim_type, '_post_processing']), 'file') == 0 &&...
+            exist(fullfile('pp_link', sim_type, 'data_postprocessed.mat'), 'file') == 0
+        run_pp = 1;
+    else
+        run_pp = 0;
+    end %if
 else
     error('Please select skip or no_skip for the postprocessing')
 end %if
