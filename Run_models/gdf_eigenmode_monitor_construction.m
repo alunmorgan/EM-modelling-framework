144e83bdd4858f6bd6000cbce9764c23499a3f0f
Alun Morgan <alun.morgan@diamond.ac.uk>
2021-05-11 09:42:42 +0100

    Pulling eigenmode passes variable to a higher level to make it user settable.

--- Run_models/gdf_eigenmode_monitor_construction.m
+++ Run_models/gdf_eigenmode_monitor_construction.m
@@ -1,4 +1,4 @@
-function fs = gdf_eigenmode_monitor_construction(estimation_freq, nsol, type, f_range)
+function fs = gdf_eigenmode_monitor_construction(estimation_freq, nsol, type, f_range, passes)
 % Constructs the initial part of the gdf input file for GdfidL
 %
 % nsol determines the number of solutions to use.
@@ -11,7 +11,7 @@ fs = cat(1,fs,'-eigenvalues');
 fs = cat(1,fs,['    solutions = ',num2str(nsol)]);
 fs = cat(1,fs,['    estimation = ', num2str(estimation_freq)]);
 fs = cat(1,fs,['    lossy = ', type]);
-fs = cat(1,fs,'    passes = 4');
+fs = cat(1,fs,['    passes = ', num2str(passes)]);
 if strcmp(type, 'yes')
     fs = cat(1,fs,['    flowsearch = ', num2str(f_range(1))]);
     fs = cat(1,fs,['    fhighsearch = ', num2str(f_range(2))]);
