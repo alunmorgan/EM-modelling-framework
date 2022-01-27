64bd3ba0052debfdfbf9bdc5b3f9dae270afa254
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-01-27 11:54:39 +0000

    Updated description.

--- Common/add_prefix.m
+++ Common/add_prefix.m
@@ -2,7 +2,7 @@ function add_prefix(root_path, filetype, prefix)
 % Scans the directory for files of the selected type. 
 % Renames them to add the prefix.
 %
-% Example: add_prefix(root_path)
+% Example: add_prefix(root_path, filetype, prefix)
 
 fullPaths = dir_list_gen(root_path, filetype, 1);
 
