597bd6daeb91accc53ebf307a122163555fd736b
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-01-27 11:58:46 +0000

    renamed variables to aid comprehension.

--- Post_processing/GdfidL_read_pp_logs.m
+++ Post_processing/GdfidL_read_pp_logs.m
@@ -1,11 +1,13 @@
 function pp_logs = GdfidL_read_pp_logs(sim_type)
 if strcmp(sim_type, 'wake')
     pp_logs = GdfidL_read_pp_wake_log(...
-        fullfile('pp_link', sim_type, ['model_',sim_type,'_post_processing_log']));
-% elseif strcmp(sim_type, 'eigenmode')
-%     run_logs = GdfidL_read_pp_eigenmode_log(...
+        fullfile('pp_link', sim_type));
+elseif strcmp(sim_type, 'eigenmode')
+    pp_logs = struct;
+%     pp_logs = GdfidL_read_pp_eigenmode_log(...
 %         fullfile('pp_link', sim_type, 'model_log'), 'eigenmode');
-% elseif strcmp(sim_type, 'lossy_eigenmode')
-%     run_logs = GdfidL_read_pp_eigenmode_log(...
+elseif strcmp(sim_type, 'lossy_eigenmode')
+        pp_logs = struct;
+%     pp_logs = GdfidL_read_pp_eigenmode_log(...
 %         fullfile('pp_link', sim_type, 'model_log'), 'lossy_eigenmode');
 end %if
