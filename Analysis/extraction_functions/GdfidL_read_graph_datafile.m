1d953658d0582331caa1bc130232d8de4179d2ab
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-02-17 10:40:11 +0000

    Extending finctions to be able to cope with absent data. For example if every thing is PEC then there are no losses. The files then only contain header information.

--- Analysis/extraction_functions/GdfidL_read_graph_datafile.m
+++ Analysis/extraction_functions/GdfidL_read_graph_datafile.m
@@ -1,61 +1,30 @@
 function [ plot_data, varargout ] = GdfidL_read_graph_datafile( file_name )
 % Reads in GdfidL graph data.
 % Returns the plot data found in the file.
-% 
+%
 % Example: [ plot_data, varargout ] = GdfidL_read_graph_datafile( file_name )
 fid = fopen(file_name);
 nd = 1;
 current_line = cell(1,1);
+%% Extracting the header information
 while strcmp(current_line, '# BEGIN_DATA') == 0 && ...
         strcmp(current_line, '## Start of Data:') == 0 && ...
         strcmp(current_line, '# start of data') == 0
     current_line = textscan(fid,'%[^\n]',1,'Delimiter','\n');
     try
-    current_line = current_line{1}{1};
+        current_line = current_line{1}{1};
     catch
-      current_line = current_line{1};  
+        current_line = current_line{1};
     end
     plot_data.header_info{nd} = current_line;
     nd = nd +1;
-end
-% put in a while loop as sometime the numerical data does not start on the next
-% line. This just retries until it gets something.
-data_temp = [];
-f_end = 0;
-while isempty(data_temp) || f_end ~= 0
-    f_loc = ftell(fid);
-    data_temp = cell2mat(textscan(fid,'%f %f', 1));  
-    fgetl(fid);
-    f_end = feof(fid);
-end
-% if the end of the file hasn't been reached
-if feof(fid) == 0
-    % Move back a line
-    fseek(fid, f_loc, 'bof');
-plot_data.data = cell2mat(textscan(fid,'%f %f'));
-nxt_line = fgetl(fid);
-end
-
-xlab_ind = find_position_in_cell_lst(strfind(plot_data.header_info,'xlabel'));
-if isempty(xlab_ind) == 0
-    xlab_locs = find_quotes_loc_in_string(plot_data.header_info{xlab_ind});
-    [plot_data.xlabel, plot_data.data] = convert_to_s(...
-        plot_data.header_info{xlab_ind}(xlab_locs(1)+1:xlab_locs(2)-1),...
-        plot_data.data);
-end
-
-ylab_ind = find_position_in_cell_lst(strfind(plot_data.header_info,'ylabel'));
-if isempty(ylab_ind) == 0
-    ylab_locs = find_quotes_loc_in_string(plot_data.header_info{ylab_ind});
-    plot_data.ylabel = ...
-        plot_data.header_info{ylab_ind}(ylab_locs(1)+1:ylab_locs(2)-1);
 end
 
 tlab_ind = find_position_in_cell_lst(strfind(plot_data.header_info,'toplabel'));
 if isempty(tlab_ind) == 0
     % If it has a generic title use the more descriptive sub title.
     if ~isempty(strfind(plot_data.header_info{tlab_ind}, 'GdfidL, 1D-Plot')) ||...
-        ~isempty(strfind(plot_data.header_info{tlab_ind}, 'GdfidL, Lineplot'))
+            ~isempty(strfind(plot_data.header_info{tlab_ind}, 'GdfidL, Lineplot'))
         tlab_ind = find_position_in_cell_lst(strfind(plot_data.header_info,'subtitle'));
     end
     tlab_locs = find_quotes_loc_in_string(plot_data.header_info{tlab_ind});
@@ -103,9 +72,48 @@ if isempty(symmetry_ind) == 0
     plot_data.symmetry = symmetry_temp{1}{1};
 end
 
-if nxt_line == -1
-    fclose(fid);
+%% Exctracting data
+% put in a while loop as sometime the numerical data does not start on the next
+% line. This just retries until it gets something.
+data_temp = [];
+f_end = 0;
+while f_end == 0 && isempty(data_temp)
+    f_loc = ftell(fid);
+    data_temp = cell2mat(textscan(fid,'%f %f', 1));
+    fgetl(fid);
+    f_end = feof(fid);
+end % while
+% if the end of the file hasn't been reached
+if f_end == 0
+    % Move back a line
+    fseek(fid, f_loc, 'bof');
+    plot_data.data = cell2mat(textscan(fid,'%f %f'));
+    nxt_line = fgetl(fid);
+    
+    
+    xlab_ind = find_position_in_cell_lst(strfind(plot_data.header_info,'xlabel'));
+    if isempty(xlab_ind) == 0
+        xlab_locs = find_quotes_loc_in_string(plot_data.header_info{xlab_ind});
+        [plot_data.xlabel, plot_data.data] = convert_to_s(...
+            plot_data.header_info{xlab_ind}(xlab_locs(1)+1:xlab_locs(2)-1),...
+            plot_data.data);
+    end %if
+    
+    ylab_ind = find_position_in_cell_lst(strfind(plot_data.header_info,'ylabel'));
+    if isempty(ylab_ind) == 0
+        ylab_locs = find_quotes_loc_in_string(plot_data.header_info{ylab_ind});
+        plot_data.ylabel = ...
+            plot_data.header_info{ylab_ind}(ylab_locs(1)+1:ylab_locs(2)-1);
+    end %if
 else
+    plot_data.ylabel = '';
+    plot_data.xlabel = '';
+    plot_data.data = [NaN, NaN];
+        fclose(fid);
+    return
+end %if
+
+if nxt_line ~= -1
     % Second dataset in file (assume charge distribution)
     if strcmp(nxt_line,  '# BEGIN_CHARGE')
         plot_data2.title = 'Charge density';
@@ -122,7 +130,7 @@ else
         plot_data2.data = cell2mat(textscan(fid,'%f %f'));
         % revert the scaling
         if scaling ~= 0
-        plot_data2.data(:,2) = plot_data2.data(:,2) ./ scaling;
+            plot_data2.data(:,2) = plot_data2.data(:,2) ./ scaling;
         end
         % convert to time units (divide the length axis by c, multiply the charge axis by c)
         plot_data2.data(:,1) = plot_data2.data(:,1) ./ 299792458;
@@ -132,13 +140,13 @@ else
         if cur_int == 0
             disp('No bunch charge!')
         else
-        plot_data2.data(:,2) = plot_data2.data(:,2) ./ cur_int;
-        end
+            plot_data2.data(:,2) = plot_data2.data(:,2) ./ cur_int;
+        end %if
         fclose(fid);
     else
         fclose(fid);
-    end
+    end %if
     varargout{1} = plot_data2;
-end
+end %if
 
 
--- Analysis/extraction_functions/extract_material_losses_from_wake_data.m
+++ Analysis/extraction_functions/extract_material_losses_from_wake_data.m
@@ -1,13 +1,18 @@
 function [model_mat_data, mat_loss, m_time, m_data] = ...
-extract_material_losses_from_wake_data(pp_data, extension_names)
+    extract_material_losses_from_wake_data(pp_data, extension_names)
 % wake data (structure): contains all the data from the wake postprocessing
 %
 % mat_loss (vector): Returns a value of total loss per material.
 %
 % Example: mat_loss = extract_material_losses_from_wake_data(wake_data)
 
-if isfield(pp_data, 'mat_losses')
+if isfield(pp_data, 'mat_losses') && iscell(pp_data.mat_losses.single_mat_data)
+    % I think that if the only material is PEC then this returns a 0 rather
+    % than a cell. In which case don't do the interpolation.
+    % TODO find the code which generates the zeros and change so that the
+    % output type is constant.
     if ~isempty(pp_data.mat_losses.loss_time)
+        
         for hsa = size(pp_data.mat_losses.single_mat_data,1):-1:1
             tmp = strcmp(extension_names, pp_data.mat_losses.single_mat_data{hsa,2});
             if sum(tmp) == 0
@@ -18,12 +23,13 @@ if isfield(pp_data, 'mat_losses')
                 model_mat_index(hsa) = 0;
             end %if
         end %for
+        
         %select on only those materials which are in the model proper.
         model_mat_data = pp_data.mat_losses.single_mat_data(model_mat_index == 1,:);
         if ~isempty(model_mat_data)
             for mes = size(model_mat_data,1):-1:1
                 mat_loss(mes) = model_mat_data{mes,4}(end,2);
-            end %for    
+            end %for
         end %if
     end %if
 else
@@ -32,7 +38,7 @@ else
 end %if
 
 % Evolution over time
-if isfield(pp_data, 'mat_losses')
+if isfield(pp_data, 'mat_losses') && iscell(pp_data.mat_losses.single_mat_data)
     if isempty(pp_data.mat_losses.loss_time) == 0
         for na = size(model_mat_data,1):-1:1
             if isempty(model_mat_data{na,4})
@@ -42,7 +48,7 @@ if isfield(pp_data, 'mat_losses')
                 m_time{na} = model_mat_data{na,4}(:,1).*1e9;
                 m_data{na} = model_mat_data{na,4}(:,2).* 1e9;
             end %if
-        end %for    
+        end %for
     end %if
 else
     m_time = {NaN};
