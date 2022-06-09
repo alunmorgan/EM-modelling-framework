8753ca4184bca9a95f4d2b6ae0b9ec7dad6a82fb
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-06-09 11:56:55 +0100

    Geometry no longer generated for off axis beam excitation models.

--- Run_models/run_models.m
+++ Run_models/run_models.m
@@ -12,86 +12,32 @@ for awh = 1:length(modelling_inputs)
     disp(datestr(now))
     disp(['Running ',num2str(awh), ' of ',...
         num2str(length(modelling_inputs)), ' simulations'])
-    
-    sims = cell(1,1);
-    restart = cell(1,1);
-    s_ck = 1;
     restart_loc_base =fullfile(restart_root, modelling_inputs{awh}.base_model_name, modelling_inputs{awh}.model_name);
-    if contains(sim_types, 'wake')
-        sims{s_ck} = 'wake';
-        restart_loc_tmp =fullfile(restart_loc_base, 'wake', '.iMod-1');
-        if exist(restart_loc_tmp)
-            restart{s_ck} = [' -restartfiles=',restart_loc_tmp, ' '];
-        else
-            restart{s_ck} = '';
-        end %if
-        s_ck = s_ck +1;
+    % only want to run the geometry model for the central beam location.
+    % remove it from the list otherwise.
+    if str2double(modelling_inputs{awh}.beam_offset_x) ~= 0 ...
+            || str2double(modelling_inputs{awh}.beam_offset_y) ~= 0
+        g_ind = find_position_in_cell_lst(strfind(sim_types, 'geometry'));
+        sim_types(g_ind) = [];
     end %if
-    if str2double(modelling_inputs{awh}.beam_offset_x) == 0 ...
-            && str2double(modelling_inputs{awh}.beam_offset_y) == 0
-        % FIXME add filter for user signals
-        if contains(sim_types, 'geometry'), ...
-                sims{s_ck} = 'geometry';
-            restart_loc_tmp =fullfile(restart_loc_base, 'geometry', '.iMod-1');
-            if exist(restart_loc_tmp)
-                restart{s_ck} = [' -restartfiles=',restart_loc_tmp, ' '];
-            else
-                restart{s_ck} = '';
-            end %if
-            s_ck = s_ck +1;
-        end %if
-        if contains(sim_types, 'sparameter')
-            sims{s_ck} = 's_parameter';
-            restart_loc_tmp =fullfile(restart_loc_base, 's_parameter', '.iMod-1');
-            if exist(restart_loc_tmp)
-                restart{s_ck} = [' -restartfiles=',restart_loc_tmp, ' '];
-            else
-                restart{s_ck} = '';
-            end %if
-            s_ck = s_ck +1;
-        end %if
-        if contains(sim_types, 'eigenmode')
-            sims{s_ck} = 'eigenmode';
-            restart_loc_tmp =fullfile(restart_loc_base, 'eigenmode', '.iMod-1');
-            if exist(restart_loc_tmp)
-                restart{s_ck} = [' -restartfiles=',restart_loc_tmp, ' '];
-            else
-                restart{s_ck} = '';
-            end %if
-            s_ck = s_ck +1;
-        end %if
-        if contains(sim_types, 'lossy_eigenmode')
-            sims{s_ck} = 'lossy_eigenmode';
-            restart_loc_tmp =fullfile(restart_loc_base, 'lossy_eigenmode', '.iMod-1');
-            if exist(restart_loc_tmp)
-                restart{s_ck} = [' -restartfiles=',restart_loc_tmp, ' '];
-            else
-                restart{s_ck} = '';
-            end %if
-            s_ck = s_ck +1;
-        end %if
-        if contains(sim_types, 'shunt')
-            sims{s_ck} = 'shunt';
-            restart_loc_tmp =fullfile(restart_loc_base, 'shunt', '.iMod-1');
-            if exist(restart_loc_tmp)
-                restart{s_ck} = [' -restartfiles=',restart_loc_tmp, ' '];
-            else
-                restart{s_ck} = '';
-            end %if
-        end %if
-    end %if
-    
-    for ksbi = 1:length(sims)
+
+    for ksbi = 1:length(sim_types)
         try
-            sim_loc = find_position_in_cell_lst(strfind(sim_types, 'wake'));
-            simulation_result_locations =  GdfidL_run_simulation(sims{ksbi}, mi.paths, modelling_inputs{awh}, ...
-                restart{ksbi});
+            %             sim_loc = find_position_in_cell_lst(strfind(sim_types, 'wake'));
+            restart_loc_tmp =fullfile(restart_loc_base, sim_types{ksbi}, '.iMod-1');
+            if exist(restart_loc_tmp, 'dir')
+                restart_line = [' -restartfiles=',restart_loc_tmp, ' '];
+            else
+                restart_line = '';
+            end %if
+            GdfidL_run_simulation(sim_types{ksbi}, mi.paths, modelling_inputs{awh}, ...
+                restart_line);
         catch ERR
-            display_modelling_error(ERR, sims{ksbi})
+            display_modelling_error(ERR, sim_types{ksbi})
             cd(default_location)
-%             if strcmp(sims{ksbi}, 's_parameter')
-%                 cd ..
-%             end %if
+            %             if strcmp(sims{ksbi}, 's_parameter')
+            %                 cd ..
+            %             end %if
             continue
         end %try
     end %for
