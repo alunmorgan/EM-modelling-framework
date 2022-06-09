56efb30817f9e7017b689393bd92dd2d94eda1ad
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-06-09 11:27:23 +0100

    adding error reporting.

--- Post_processing/cleanup_after_pp.m
+++ Post_processing/cleanup_after_pp.m
@@ -1,7 +1,11 @@
 function cleanup_after_pp(old_loc, tmp_name)
 
 %% Remove the links and move back to the original directory.
+try 
 delete('pp_link')
 delete('data_link')
+catch ME
+    disp(ME)
+end %try
 cd(old_loc)
 rmdir(fullfile(tmp_name),'s');
