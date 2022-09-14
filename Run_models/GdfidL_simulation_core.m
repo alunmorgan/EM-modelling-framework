ce22eca8111b09bc7ce4e0cd2c636d2c0795e760
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-09-14 08:53:11 +0100

    Adding code to cope with filesystem delays.

--- Run_models/GdfidL_simulation_core.m
+++ Run_models/GdfidL_simulation_core.m
@@ -10,13 +10,33 @@ shell_contents = {'#! /bin/bash'};
 % problems were found with differences in the shell vs matlab environment.
 % As a result this code now writes and executes a shell script so that the
 % system environment is reliably used.
+shell_contents = cat(1, shell_contents, 'ls -ah');
 if strcmp(precision, 'single')
     shell_contents = cat(1,shell_contents,['single.gd1 ',restart,'< model.gdf > model_log']);
 elseif strcmp(precision, 'double')
     shell_contents = cat(1,shell_contents,['gd1 ',restart,'< model.gdf > model_log']);
 end %if
 write_out_data( shell_contents, 'run_model.sh' )
-[~] = system('chmod +x run_model.sh');
-!./run_model.sh
+pause(5)
+[status, cmd_out] = fileattrib('run_model.sh', '+x', 'a');
+if status == 0
+    disp(['ERROR setting permissions on run file', cmd_out])
+    % probably a slow file system update
+    disp('Waiting for filesystem')
+    pause(5)
+    [status, cmd_out] = fileattrib('run_model.sh', '+x', 'a');
+    if status == 1
+        pause(20) % problem with slow filesystem update
+        dir
+        !./run_model.sh
+    else
+        disp(['ERROR setting permissions on run file', cmd_out])
+    end %if
+else
+    pause(20) % problem with slow filesystem update
+    dir
+    !./run_model.sh
+    
+end %if
 % restoring the original version.
 setenv('GDFIDL_VERSION',orig_ver);
