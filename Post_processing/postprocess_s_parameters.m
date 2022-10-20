af16a5e07bb92003e74c8ddd59ad879be1b37607
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-10-20 10:45:29 +0100

    Adding some preallocation

--- Post_processing/postprocess_s_parameters.m
+++ Post_processing/postprocess_s_parameters.m
@@ -10,6 +10,8 @@ function postprocess_s_parameters(data_directory, output_directory)
 %% S PARAMETER POSTPROCESSING
 % creating_space_for_postprocessing(fullfile('pp_link', 'sparameter'), 'sparameter', model_name);
 [s_names, ~] = dir_list_gen(data_directory, 'dirs', 1);
+s_parameter_data_directory = cell(length(s_names),1);
+s_parameter_output_directory = cell(length(s_names),1);
 for osw = 1:length(s_names)
         s_parameter_data_directory{osw} = fullfile(data_directory, s_names{osw});
         s_parameter_output_directory{osw} = fullfile(output_directory, s_names{osw});
@@ -18,9 +20,9 @@ end %for
 
 s_port = cell(length(s_names),1);
 % finding the names of all the ports.
-[temp, ~]  = dir_list_gen(fullfile(data_directory, s_names{1}), 'dirs', 1);
-temp = temp( find_position_in_cell_lst((strfind(temp, 'Port='))));
-all_ports = regexprep(temp, 'Port=','');
+% [temp, ~]  = dir_list_gen(fullfile(data_directory, s_names{1}), 'dirs', 1);
+% temp = temp( find_position_in_cell_lst((strfind(temp, 'Port='))));
+% all_ports = regexprep(temp, 'Port=','');
 
 for osw = 1:length(s_names)
 
@@ -42,8 +44,8 @@ for osw = 1:length(s_names)
     s_set = regexp(s_parameter_data_directory{osw}, 'set_(.*)_port_.*_excitation', 'tokens');
     s_set = s_set{1}{1};
     s_port{osw} = excite;
-    set{osw} = s_set;
-    pp_s_input_file = GdfidL_write_pp_s_param_input_file(s_parameter_data_directory{osw}, s_set, excite);
+%     set{osw} = s_set;
+    pp_s_input_file = GdfidL_write_pp_s_param_input_file(s_parameter_data_directory{osw});
     pp_output_name = fullfile(s_parameter_output_directory{osw} ,['model_s_param_set_',s_set, '_',excite,'_post_processing_input_file']);
     write_out_data(pp_s_input_file, pp_output_name )
     
