1570e52bb761f63eeb12cdcf38d6720c9c12e690
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-03-23 14:03:12 +0000

    Increased the resolution of the counting.

--- Common/read_in_text_file.m
+++ Common/read_in_text_file.m
@@ -12,12 +12,12 @@ nrows = numel(cell2mat(textscan(fid,'%1c%*[^\n]')));
 frewind(fid);
 data = cell(nrows,1);
 pgs = 0;
-fprintf(' 0%%')
+fprintf('  0%%')
 for nd = 1:nrows
     current_line = fgetl(fid);
     if rem(nd, floor(nrows/100)) == 0
         pgs = pgs + 1;
-        fprintf(['\b\b\b', num2str(pgs, '%02.f'),'%%'])
+        fprintf(['\b\b\b\b', num2str(pgs, '%03.f'),'%%'])
     end %if
     if current_line == -1
         break
@@ -26,6 +26,6 @@ for nd = 1:nrows
 end
 fclose(fid);
 data = data';
-fprintf('\n')
+
 
 
