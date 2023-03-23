617407b988a0f5006b4f6e1d1ee0d40acfb8f2bb
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-03-23 14:13:43 +0000

    removing the save on fail as this is better  done elsewhere.

--- plotting/write_vid.m
+++ plotting/write_vid.m
@@ -1,14 +1,10 @@
 function write_vid(data, output_loc)
-try
-    v = VideoWriter(output_loc);
-    v.FrameRate = 5;
-    open(v);
-    for kwh = 1:length(data)
-        writeVideo(v, data(kwh));
-    end %for
-    close(v)
-catch
-    save(output_loc, 'data')
-end %try
+% Writes video from input frames file.
 
-end %function
+v = VideoWriter(output_loc);
+v.FrameRate = 5;
+open(v);
+for kwh = 1:length(data)
+    writeVideo(v, data(kwh));
+end %for
+close(v)
